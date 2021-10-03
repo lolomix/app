29a2b8fbf70d55e371e3a8cf8f94a44270f40719
Simon Buechi <simon.buechi@gmail.com>
2021-10-03 18:41:24 +0200

    setup assetlist for kitchen

---
+++ src/views/Kitchen/assetList.js
@@ -0,0 +1,5 @@
+export const assetList = [
+  { code: "btc", name: "Bitcoin", description: "", active: true, icon: "./icons/btc.png" },
+  { code: "eth", name: "Ethereum",  description: "", active: true, icon: "./icons/eth.png"},
+  { code: "matic", name: "Matic",  description: "", active: true, icon: "./icons/matic.png" },
+];
