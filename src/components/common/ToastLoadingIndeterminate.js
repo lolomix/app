414c298272b28848b7c5bba37ac8e7cbaf5b6b1d
rolandsaven <rolandka@live.com>
2021-10-03 20:53:17 +0100

    remove unused components

--- src/components/common/ToastLoadingIndeterminate.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-//material-ui
-import CircularProgress from "@mui/material/CircularProgress";
-
-export default function ToastLoadingIndeterminate() {
-  return <CircularProgress variant="indeterminate" color="secondary" size={30} thickness={2} />;
-}
