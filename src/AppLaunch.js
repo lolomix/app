38a7e05101f21e3e22e0695d485fb7580582c5cb
Simon Buechi <simon.buechi@gmail.com>
2021-10-03 17:34:11 +0200

    add logo to app loader page (before app is fully loaded)

--- src/AppLaunch.js
+++ src/AppLaunch.js
@@ -1,10 +1,18 @@
 import React, { Component } from "react";
+import logo from "./assets/logo.png";
 
 class AppLaunch extends Component {
   render() {
     return (
-      <div>
-        <p>Loading...</p>
+      <div style={{
+        margin: 0,
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)"
+      }}>
+        <img src={logo} width={400} height={400} alt="CryptoChefs Logo" />
+        <p style={{textAlign:"center"}}>Loading...</p>
       </div>
     );
   }
