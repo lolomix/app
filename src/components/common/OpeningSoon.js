80e666932baaf469e093ac52e6e3833dda2e70e7
Simon Buechi <simon.buechi@gmail.com>
2021-10-06 08:46:13 +0200

    add stylized openingsoon component

---
+++ src/components/common/OpeningSoon.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "./openingsoon.css";
+
+class OpeningSoon extends Component {
+
+  render() {
+
+    return (
+      <div className="openingsoon">
+        <link href='https://fonts.googleapis.com/css?family=Sacramento:400' rel='stylesheet' type='text/css'>
+        <h1 className="neontext">
+          opening soon
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default OpeningSoon;
