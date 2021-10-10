690e4b750b07bf9f2468f11253601a9edf657002
Simon Buechi <simon.buechi@gmail.com>
2021-10-10 20:31:40 +0200

    make store the main page for now

--- src/routes.js
+++ src/routes.js
@@ -1,24 +1,24 @@
-import React, { Component, Suspense, lazy } from 'react'
-import { Switch, Route, BrowserRouter as Router } from 'react-router-dom'
-import { withTranslation } from 'react-i18next'
-import { SnackbarProvider } from 'notistack'
-import { Web3ReactProvider } from '@web3-react/core'
-import Web3 from 'web3'
+import React, { Component, Suspense, lazy } from "react";
+import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
+import { withTranslation } from "react-i18next";
+import { SnackbarProvider } from "notistack";
+import { Web3ReactProvider } from "@web3-react/core";
+import Web3 from "web3";
 // fonts
-import '@fontsource/roboto/300.css'
-import '@fontsource/roboto/400.css'
-import '@fontsource/roboto/500.css'
-import '@fontsource/roboto/700.css'
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 // material-ui
-import { green, blue, red } from '@mui/material/colors'
-import { ThemeProvider } from '@mui/material/styles'
-import CssBaseline from '@mui/material/CssBaseline'
-import { theme } from './utils/theme'
+import { green, blue, red } from "@mui/material/colors";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { theme } from "./utils/theme";
 // shell
-import TopBar from './components/layout/TopBar'
-import LoadingSpinner from './components/common/LoadingSpinner'
-import ServiceWorkerWrapper from './components/ServiceWorkerWrapper'
-import { Container, Paper } from '@mui/material'
+import TopBar from "./components/layout/TopBar";
+import LoadingSpinner from "./components/common/LoadingSpinner";
+import ServiceWorkerWrapper from "./components/ServiceWorkerWrapper";
+import { Container, Paper } from "@mui/material";
 // pages (lazy loading)
 const Main = lazy(() => import("./views/Main/Main"));
 //const Web3Test = lazy(() => import("./views/Web3/Web3Test"));
@@ -26,10 +26,10 @@ const Store = lazy(() => import("./views/Store/Store"));
 const Kitchen = lazy(() => import("./views/Kitchen/Kitchen"));
 const Buffet = lazy(() => import("./views/Buffet/Buffet"));
 
-function getLibrary (provider) {
+function getLibrary(provider) {
   //const library =
   //library.pollingInterval = 12000
-  return new Web3(provider)
+  return new Web3(provider);
 }
 
 class App extends Component {
@@ -37,59 +37,56 @@ class App extends Component {
     expertmode: false,
     videoOn: false,
     admin: false,
-  }
+  };
 
   setExpertMode = (boolValue) => {
-    this.setState({ expertmode: boolValue })
-    window.localStorage.setItem('expertmode', boolValue)
-  }
+    this.setState({ expertmode: boolValue });
+    window.localStorage.setItem("expertmode", boolValue);
+  };
   setVideoOn = (boolValue) => {
-    this.setState({ videoOn: boolValue })
-    window.localStorage.setItem('videoOn', boolValue)
-  }
+    this.setState({ videoOn: boolValue });
+    window.localStorage.setItem("videoOn", boolValue);
+  };
   setAdmin = (boolValue) => {
-    this.setState({ admin: boolValue })
-  }
+    this.setState({ admin: boolValue });
+  };
 
-  render () {
-    const { expertmode, videoOn, admin } = this.state
+  render() {
+    const { expertmode, videoOn, admin } = this.state;
 
     return (
       <Web3ReactProvider getLibrary={getLibrary}>
         <ThemeProvider theme={theme}>
           <SnackbarProvider
             anchorOrigin={{
-              vertical: 'bottom',
-              horizontal: 'right',
+              vertical: "bottom",
+              horizontal: "right",
             }}
             maxSnack={3}
             autoHideDuration={6000}
             classes={{
               variantSuccess: {
-                backgroundImage: 'linear-gradient(90deg,' + green[700] + ',' + green[500] + ')',
+                backgroundImage: "linear-gradient(90deg," + green[700] + "," + green[500] + ")",
               },
               variantError: {
-                backgroundImage: 'linear-gradient(90deg,' + red[700] + ',' + red[400] + ')',
+                backgroundImage: "linear-gradient(90deg," + red[700] + "," + red[400] + ")",
               },
               variantWarning: {
-                backgroundImage: 'linear-gradient(90deg,' + blue[700] + ',' + blue[500] + ')',
+                backgroundImage: "linear-gradient(90deg," + blue[700] + "," + blue[500] + ")",
               },
               variantInfo: {
-                backgroundImage: 'linear-gradient(90deg,' + blue[700] + ',' + blue[500] + ')',
+                backgroundImage: "linear-gradient(90deg," + blue[700] + "," + blue[500] + ")",
               },
             }}>
-            <CssBaseline/>
+            <CssBaseline />
             <Router basename="/">
               <Container maxWidth="lg" disableGutters={true}>
                 <Paper square elevation={5}>
-                  <TopBar setExpertMode={this.setExpertMode}
-                          setVideoOn={this.setVideoOn} expertmode={expertmode}
-                          videoOn={videoOn} admin={admin}
-                          />
-                  <ServiceWorkerWrapper/>
-                  <Suspense fallback={<LoadingSpinner/>}>
+                  <TopBar setExpertMode={this.setExpertMode} setVideoOn={this.setVideoOn} expertmode={expertmode} videoOn={videoOn} admin={admin} />
+                  <ServiceWorkerWrapper />
+                  <Suspense fallback={<LoadingSpinner />}>
                     <Switch>
-                      <Route exact path="/" component={Main} />
+                      <Route exact path="/" component={Store} />
                       <Route exact path="/start" component={Main} />
                       <Route exact path="/buffet" component={Buffet} />
                       <Route exact path="/store" component={Store} />
@@ -103,8 +100,8 @@ class App extends Component {
           </SnackbarProvider>
         </ThemeProvider>
       </Web3ReactProvider>
-    )
+    );
   }
 }
 
-export default withTranslation()(App)
+export default withTranslation()(App);
