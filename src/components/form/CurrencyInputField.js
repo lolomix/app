608b736b6ded9d0801c03c047459a3556c7c6f71
rolandsaven <rolandka@live.com>
2021-10-14 20:16:28 +0100

    update input field label

--- src/components/form/CurrencyInputField.js
+++ src/components/form/CurrencyInputField.js
@@ -19,36 +19,31 @@ function CurrencyInputField({ id, label, currency, onUserInput, value, disabled 
   const CurrencyIcon = currencyIconComponents[currency];
 
   return (
-    <FormControl fullWidth>
-      <InputLabel htmlFor={id} shrink={true}>
-        {label}
-      </InputLabel>
-      <OutlinedInput
-        id={id}
-        value={value}
-        onChange={onUserInput}
-        sx={{
-          backgroundColor: "#fff",
-        }}
-        inputProps={{
-          inputMode: "numeric",
-          pattern: "[0-9]*",
-        }}
-        endAdornment={
-          <InputAdornment position="end">
-            <Typography pr={1} fontWeight={500}>
-              {currency}
-            </Typography>
-            <CurrencyIcon
-              fontSize="large"
-              sx={{
-                border: "2px solid #f1f1f1",
-                borderRadius: "100%",
-              }}
-            />
-          </InputAdornment>
-        }
-        disabled={disabled}
+    <FormControl variant="outlined" fullWidth>
+      <Typography variant="overline">{label}</Typography>
+      <OutlinedInput id={id}
+                     value={value}
+                     onChange={onUserInput}
+                     placeholder={0}
+                     sx={{
+                        backgroundColor: '#fff'
+                     }}
+                     inputProps={{
+                       inputMode: 'numeric',
+                       pattern: '[0-9]*'
+                     }}
+                     endAdornment={
+                       <InputAdornment position="end">
+                         <Typography pr={1} fontWeight={500}>
+                           {currency}
+                         </Typography>
+                         <CurrencyIcon fontSize="large" sx={{
+                           border: "2px solid #f1f1f1",
+                           borderRadius: "100%"
+                         }}/>
+                       </InputAdornment>
+                     }
+                     disabled={disabled}
       />
     </FormControl>
   );
