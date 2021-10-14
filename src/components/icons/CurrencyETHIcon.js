e08d27908aa2d1f8d4474f7a5c71961f26eaa7e1
rolandsaven <rolandka@live.com>
2021-10-14 21:46:20 +0100

    update eth svg icon

--- src/components/icons/CurrencyETHIcon.js
+++ src/components/icons/CurrencyETHIcon.js
@@ -4,37 +4,28 @@ import { SvgIcon } from '@mui/material'
 
 function CurrencyETHIcon(props) {
   return (
-    <SvgIcon viewBox="0 0 1242 962" x="0" y="0" {...props}>
-      <g id="All">
-        <g id="Developer-Center" transform="translate(-1008 -2285)">
-          <g id="Group-28" transform="translate(382 2270.8)">
-            <g id="Group-29" transform="translate(0 .2)">
-              <g id="Group-10" transform="translate(0 14)">
-                <g id="Group-13">
-                  <g id="Group-46" transform="translate(626.4)">
-                    <g id="Group-45">
-                      <circle id="Oval" cx="620.8" cy="481.5" r="365.8"
-                              fill="#627eea"/>
-                      <path id="Path" fill="#fff" fill-opacity=".602"
-                            d="M632.2 207.1v202.8l171.4 76.6z"/>
-                      <path id="Path_1_" fill="#fff"
-                            d="M632.2 207.1 460.8 486.5l171.4-76.6z"/>
-                      <path id="Path_2_" fill="#fff" fill-opacity=".602"
-                            d="M632.2 617.9v137.8l171.5-237.3z"/>
-                      <path id="Path_3_" fill="#fff"
-                            d="M632.2 755.7V617.9l-171.4-99.5z"/>
-                      <path id="Path_4_" fill="#fff" fill-opacity=".2"
-                            d="m632.2 586 171.4-99.5-171.4-76.6z"/>
-                    </g>
-                    <path id="Path_5_" fill="#fff" fill-opacity=".602"
-                          d="M460.8 486.5 632.2 586V409.9z"/>
-                  </g>
-                </g>
-              </g>
-            </g>
-          </g>
-        </g>
+    <SvgIcon viewBox="0 0 731.58 731.58" {...props}>
+      <defs>
+        <style>
+          {`.C6B2sGGpIk,.mVKY1kgP3Y{fill:#fff}.C6B2sGGpIk{fill-opacity:.6}`}
+        </style>
+      </defs>
+      <g id="Group-45">
+        <circle id="Oval" cx="365.79" cy="365.79" r="365.79" fill="#627eea"/>
+        <path id="Path" d="M377.18 91.45v202.78l171.39 76.59L377.18 91.45z"
+              className="C6B2sGGpIk"/>
+        <path id="Path-2" d="M377.18 91.45 205.76 370.82l171.42-76.59V91.45z"
+              className="mVKY1kgP3Y" data-name="Path"/>
+        <path id="Path-3" d="M377.18 502.23v137.79l171.5-237.28-171.5 99.49z"
+              className="C6B2sGGpIk" data-name="Path"/>
+        <path id="Path-4" d="M377.18 640.02V502.21l-171.42-99.47 171.42 237.28z"
+              className="mVKY1kgP3Y" data-name="Path"/>
+        <path id="Path-5" fill="#fff" fill-opacity=".2"
+              d="m377.18 470.34 171.39-99.52-171.39-76.54v176.06z"
+              data-name="Path"/>
       </g>
+      <path id="Path-6" d="m205.76 370.82 171.42 99.52V294.28l-171.42 76.54z"
+            className="C6B2sGGpIk" data-name="Path"/>
     </SvgIcon>
   );
 }
