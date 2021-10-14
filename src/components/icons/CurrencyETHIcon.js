cd2c3443626bba8921a3ca99bbdc0b3cec2d06e4
rolandsaven <rolandka@live.com>
2021-10-14 20:12:22 +0100

    add eth currency icon svg

---
+++ src/components/icons/CurrencyETHIcon.js
@@ -0,0 +1,42 @@
+import * as React from 'react';
+// material-ui
+import { SvgIcon } from '@mui/material'
+
+function CurrencyETHIcon(props) {
+  return (
+    <SvgIcon viewBox="0 0 1242 962" x="0" y="0" {...props}>
+      <g id="All">
+        <g id="Developer-Center" transform="translate(-1008 -2285)">
+          <g id="Group-28" transform="translate(382 2270.8)">
+            <g id="Group-29" transform="translate(0 .2)">
+              <g id="Group-10" transform="translate(0 14)">
+                <g id="Group-13">
+                  <g id="Group-46" transform="translate(626.4)">
+                    <g id="Group-45">
+                      <circle id="Oval" cx="620.8" cy="481.5" r="365.8"
+                              fill="#627eea"/>
+                      <path id="Path" fill="#fff" fill-opacity=".602"
+                            d="M632.2 207.1v202.8l171.4 76.6z"/>
+                      <path id="Path_1_" fill="#fff"
+                            d="M632.2 207.1 460.8 486.5l171.4-76.6z"/>
+                      <path id="Path_2_" fill="#fff" fill-opacity=".602"
+                            d="M632.2 617.9v137.8l171.5-237.3z"/>
+                      <path id="Path_3_" fill="#fff"
+                            d="M632.2 755.7V617.9l-171.4-99.5z"/>
+                      <path id="Path_4_" fill="#fff" fill-opacity=".2"
+                            d="m632.2 586 171.4-99.5-171.4-76.6z"/>
+                    </g>
+                    <path id="Path_5_" fill="#fff" fill-opacity=".602"
+                          d="M460.8 486.5 632.2 586V409.9z"/>
+                  </g>
+                </g>
+              </g>
+            </g>
+          </g>
+        </g>
+      </g>
+    </SvgIcon>
+  );
+}
+
+export default CurrencyETHIcon
