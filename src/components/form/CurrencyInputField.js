9d3689c9970a930d54292781f9432cda38d19ab1
Simon Buechi <simon.buechi@gmail.com>
2021-10-14 09:10:02 +0200

    fix placeholder warning on inputfield

--- src/components/form/CurrencyInputField.js
+++ src/components/form/CurrencyInputField.js
@@ -1,55 +1,53 @@
-import React from 'react'
-import { withTranslation } from 'react-i18next'
+import React from "react";
+import { withTranslation } from "react-i18next";
 // material-ui
-import {
-  FormControl,
-  InputAdornment,
-  InputLabel,
-  OutlinedInput,
-  Typography,
-} from '@mui/material'
-import CurrencyMaticIcon from '../icons/CurrencyMaticIcon'
-import CurrencyAromaIcon from '../icons/CurrencyAromaIcon'
+import { FormControl, InputAdornment, InputLabel, OutlinedInput, Typography } from "@mui/material";
+import CurrencyMaticIcon from "../icons/CurrencyMaticIcon";
+import CurrencyAromaIcon from "../icons/CurrencyAromaIcon";
 
-function CurrencyInputField({ id, label, currency, onUserInput, value, disabled}) {
+function CurrencyInputField({ id, label, currency, onUserInput, value, disabled }) {
   const currencyIconComponents = {
-    'MATIC': CurrencyMaticIcon,
-    'AROMA': CurrencyAromaIcon
-  }
+    MATIC: CurrencyMaticIcon,
+    AROMA: CurrencyAromaIcon,
+  };
 
   // dynamically reference the icon component
   const CurrencyIcon = currencyIconComponents[currency];
 
   return (
     <FormControl fullWidth>
-      <InputLabel htmlFor={id} shrink={true}>{label}</InputLabel>
-      <OutlinedInput id={id}
-                     value={value}
-                     onChange={onUserInput}
-                     placeholder={0}
-                     sx={{
-                        backgroundColor: '#fff'
-                     }}
-                     inputProps={{
-                       inputMode: 'numeric',
-                       pattern: '[0-9]*'
-                     }}
-                     endAdornment={
-                       <InputAdornment position="end">
-                         <Typography pr={1} fontWeight={500}>
-                           {currency}
-                         </Typography>
-                         <CurrencyIcon fontSize="large" sx={{
-                           border: "2px solid #f1f1f1",
-                           borderRadius: "100%"
-                         }}/>
-                       </InputAdornment>
-                     }
-                     disabled={disabled}
+      <InputLabel htmlFor={id} shrink={true}>
+        {label}
+      </InputLabel>
+      <OutlinedInput
+        id={id}
+        value={value}
+        onChange={onUserInput}
+        sx={{
+          backgroundColor: "#fff",
+        }}
+        inputProps={{
+          inputMode: "numeric",
+          pattern: "[0-9]*",
+        }}
+        endAdornment={
+          <InputAdornment position="end">
+            <Typography pr={1} fontWeight={500}>
+              {currency}
+            </Typography>
+            <CurrencyIcon
+              fontSize="large"
+              sx={{
+                border: "2px solid #f1f1f1",
+                borderRadius: "100%",
+              }}
+            />
+          </InputAdornment>
+        }
+        disabled={disabled}
       />
     </FormControl>
-  )
+  );
 }
 
-export default withTranslation()(CurrencyInputField)
-
+export default withTranslation()(CurrencyInputField);
