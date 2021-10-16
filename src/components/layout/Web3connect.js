9d1396e28a3c3011b30d00e2b74f53676f38cb09
rolandsaven <rolandka@live.com>
2021-10-16 13:51:13 +0100

    refactor of wallet dialog component

--- src/components/layout/Web3connect.js
+++ src/components/layout/Web3connect.js
@@ -1,4 +1,4 @@
-import React, { useState, Fragment, useEffect } from "react";
+import React, { useState, Fragment } from "react";
 import { withTranslation } from "react-i18next";
 import { withRouter } from "react-router-dom";
 import Blockies from "react-blockies";
@@ -14,10 +14,10 @@ import Button from "@mui/material/Button";
 import Popover from "@mui/material/Popover";
 import Avatar from "@mui/material/Avatar";
 import IconButton from "@mui/material/IconButton";
-import Backdrop from "@mui/material/Backdrop";
 import Box from "@mui/material/Box";
 import Stack from '@mui/material/Stack'
 import Dialog from "@mui/material/Dialog";
+import DialogTitle from '@mui/materialDialogTitle';
 import DialogContent from "@mui/material/DialogContent";
 import DialogActions from "@mui/material/DialogActions";
 import Typography from "@mui/material/Typography";
@@ -25,6 +25,7 @@ import Grid from "@mui/material/Grid";
 import CheckIcon from '@mui/icons-material/Check';
 import WarningIcon from '@mui/icons-material/Warning';
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
 //custom
 import connectorsList from "../../web3/connectorsList";
 import { getErrorMessage } from "../../web3/errors";
@@ -32,17 +33,23 @@ import { useEagerConnect } from "../../web3/hooks";
 import Balance from "../web3/Balance";
 import AromaBalance from "../web3/AromaBalance";
 import { NETWORKS, TARGET_CHAIN } from "../../web3/constants";
+import LoadingButton from '@mui/lab/LoadingButton'
+
 
 //import ToastLoading from "../../components/notification/ToastLoading";
 //import ToastLoadingIndeterminate from "../../components/notification/ToastLoadingIndeterminate";
 
 function Web3connect(props) {
+  const { t } = props;
+
   const Web3Context = useWeb3React();
   const triedEager = useEagerConnect();
   const { connector, account, activate, deactivate, active, error, chainId } = Web3Context;
-  const [activatingConnector, setActivatingConnector] = React.useState();
-  const [connectionMenu, setconnectionMenu] = useState(false);
-  const [dialogWeb3, setdialogWeb3] = useState(false);
+
+
+  const [activatingConnector, setActivatingConnector] = useState();
+  const [connectionMenu, setConnectionMenu] = useState(false);
+  const [dialogWeb3, setDialogWeb3] = useState(false);
   // const { enqueueSnackbar, closeSnackbar } = useSnackbar();
 
   /**
@@ -71,41 +78,73 @@ function Web3connect(props) {
       document.body.removeChild(ele)
     }
   };
+
   const handleConnectionIconClick = (event) => {
-    setconnectionMenu(event.currentTarget);
+    setConnectionMenu(event.currentTarget);
   };
+
   const handleConnectionMenuClose = () => {
-    setconnectionMenu(null);
+    setConnectionMenu(null);
   };
+
   const handleWeb3Modal = () => {
-    setconnectionMenu(null);
-    setdialogWeb3(!dialogWeb3);
+    setConnectionMenu(null);
+    setDialogWeb3(dialogWeb3 => !dialogWeb3);
   };
-  const { t } = props;
 
   const loadWeb3Modal = () => {
-    setdialogWeb3(!dialogWeb3);
+    setDialogWeb3(dialogWeb3 => !dialogWeb3);
   };
 
-  useEffect(() => {
-    if (activatingConnector && activatingConnector === connector) {
+  /**
+   * Identifies the current connector by key
+   *
+   * @param connectorKey
+   * @returns {boolean}
+   */
+  const isCurrentConnector = (connectorKey) =>
+    connectorsList[connectorKey]?.connector === connector
+
+  /**
+   * Activates a new connector by its key
+   *
+   * @param connectorKey
+   */
+  const handleConnectorButtonClick = (connectorKey) => {
+    const newConnector = connectorsList[connectorKey].connector
+
+    setActivatingConnector(newConnector);
+    activate(newConnector).then(() => {
+      handleWeb3Modal()
       setActivatingConnector(undefined);
-    }
-  }, [activatingConnector, connector]);
-  /*
-  useEffect(() => {
-    if (!!error) {
-      enqueueSnackbar("I love hooks");
-    }
-  }, [error, enqueueSnackbar]);
-*/
+    });
+  }
+
+  /**
+   * @param connectorKey
+   * @returns {boolean}
+   */
+  const handleConnectorButtonLoadingProp = (connectorKey) =>
+    connectorsList[connectorKey].connector === activatingConnector
+
+  /**
+   * @param connectorKey
+   * @returns {boolean}
+   */
+  const handleConnectorButtonDisabledProp = (connectorKey) => {
+    return (
+      !triedEager ||
+      !!activatingConnector ||
+      !!error
+    );
+  }
 
   return (
     <Fragment>
       {!account ? (
-        <Tooltip title="Connect to your Ethereum account">
+        <Tooltip title={t("base.connectToYourWallet")}>
           <Button elongatedWidth color="secondary" variant="contained" onClick={loadWeb3Modal}>
-            Connect
+            {t("base.connect")}
           </Button>
         </Tooltip>
       ) : (
@@ -117,108 +156,123 @@ function Web3connect(props) {
               </Avatar>
             </IconButton>
           </Tooltip>
-          <Backdrop open={Boolean(connectionMenu)} className="backdropZindex">
-            <Popover id="settings-menu" open={Boolean(connectionMenu)} anchorEl={connectionMenu} onClose={handleConnectionMenuClose}>
-              <Box p={2}>
-                <List dense>
-                  {account && (
-                    <ListItem
-                      secondaryAction={
-                        <Tooltip title={t("base.copyToClipboard")}>
-                          <IconButton onClick={handleAccountCopyToClipboardIconClick} edge="end" aria-label="copy">
-                            <ContentCopyIcon />
-                          </IconButton>
-                        </Tooltip>
-                      }>
-                      <Tooltip disableFocusListener title={t("base.youraccount")}>
-                        <ListItemAvatar>
-                          <Avatar>
-                            <Blockies seed={account.toLowerCase()} size={10} scale={4} className="blockies" />
-                          </Avatar>
-                        </ListItemAvatar>
+          <Popover id="settings-menu"
+                   open={Boolean(connectionMenu)}
+                   anchorEl={connectionMenu}
+                   onClose={handleConnectionMenuClose}
+                   anchorOrigin={{
+                     vertical: 'bottom',
+                     horizontal: 'right',
+                   }}
+          >
+            <Box p={2}>
+              <List dense>
+                {account && (
+                  <ListItem
+                    secondaryAction={
+                      <Tooltip title={t("base.copyToClipboard")}>
+                        <IconButton onClick={handleAccountCopyToClipboardIconClick} edge="end" aria-label="copy">
+                          <ContentCopyIcon />
+                        </IconButton>
                       </Tooltip>
-                      <ListItemText secondary={prettifyAccountAddress(account.toLowerCase())} primary={t("base.yourAddress")} />
-                    </ListItem>
-                  )}
-                  {chainId === NETWORKS[TARGET_CHAIN].chainId ? (
-                    <ListItem>
+                    }>
+                    <Tooltip disableFocusListener title={t("base.youraccount")}>
                       <ListItemAvatar>
-                        <Avatar className="avatar-success">
-                          <CheckIcon color="tertiary"/>
+                        <Avatar>
+                          <Blockies seed={account.toLowerCase()} size={10} scale={4} className="blockies" />
                         </Avatar>
                       </ListItemAvatar>
-                      <ListItemText secondary={TARGET_CHAIN.toUpperCase()} primary="Connection Established" />
-                    </ListItem>
-                  ) : (
-                    <ListItem>
-                      <ListItemAvatar>
-                        <Avatar className="avatar-warning">
-                          <WarningIcon />
-                        </Avatar>
-                      </ListItemAvatar>
-                      <ListItemText secondary={"Please select " + TARGET_CHAIN.toUpperCase() + " in your wallet"} primary="WRONG NETWORK" />
-                    </ListItem>
-                  )}
-                  <ListItem>
-                    <ListItemText secondary={<AromaBalance />} primary="Your AROMA balance" />
+                    </Tooltip>
+                    <ListItemText secondary={prettifyAccountAddress(account.toLowerCase())} primary={t("base.yourAddress")} />
                   </ListItem>
+                )}
+                {chainId === NETWORKS[TARGET_CHAIN].chainId ? (
                   <ListItem>
-                    <ListItemText secondary={<Balance />} primary="Your MATIC balance" />
+                    <ListItemAvatar>
+                      <Avatar className="avatar-success">
+                        <CheckIcon color="tertiary"/>
+                      </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText secondary={TARGET_CHAIN.toUpperCase() + ' network'} primary="Connection Established" />
                   </ListItem>
-                </List>
-                <Stack spacing={1} direction="row" justifyContent="center">
-                  <Button disableElevation color="primary" variant="contained" onClick={handleConnectionMenuClose}>
-                    {t("base.close")}
-                  </Button>
-                  <Button color="primary" variant="outlined" onClick={handleWeb3Modal}>
-                    {t("base.settings")}
-                  </Button>
-                </Stack>
-              </Box>
-            </Popover>
-          </Backdrop>
+                ) : (
+                  <ListItem>
+                    <ListItemAvatar>
+                      <Avatar className="avatar-warning">
+                        <WarningIcon />
+                      </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText secondary={"Please select " + TARGET_CHAIN.toUpperCase() + " in your wallet"} primary="WRONG NETWORK" />
+                  </ListItem>
+                )}
+                <ListItem>
+                  <ListItemText secondary={<AromaBalance />} primary="Your AROMA balance" />
+                </ListItem>
+                <ListItem>
+                  <ListItemText secondary={<Balance />} primary="Your MATIC balance" />
+                </ListItem>
+              </List>
+              <Stack spacing={1} direction="row" justifyContent="center">
+                <Button disableElevation color="primary" variant="contained" onClick={handleConnectionMenuClose}>
+                  {t("base.close")}
+                </Button>
+                <Button color="primary" variant="outlined" onClick={handleWeb3Modal}>
+                  {t("base.settings")}
+                </Button>
+              </Stack>
+            </Box>
+          </Popover>
         </>
       )}
-      <Dialog onClose={handleWeb3Modal} open={dialogWeb3} keepMounted maxWidth="lg">
+      <Dialog
+        keepMounted
+        onClose={handleWeb3Modal}
+        open={dialogWeb3}
+        scroll="paper"
+        maxWidth="sm"
+        aria-labelledby="wallet-connectors-dialog-title"
+        aria-describedby="wallet-connectors-dialog-description"
+      >
+        <DialogTitle id="wallet-connectors-dialog-title">
+          {t("base.connectToYourWallet")}
+        </DialogTitle>
         <DialogContent>
-          <Typography variant="h2" gutterBottom>
-            Connect Wallet
-          </Typography>
-
-          {Object.keys(connectorsList).map((name) => (
-            <Box mb={2} key={name}>
-              <Grid container spacing={2}>
-                <Grid item sm={6} md={4}>
-                  <Button
-                    variant="contained"
-                    style={{
-                      borderColor:
-                        connectorsList[name].connector === activatingConnector ? "orange" : connectorsList[name].connector === connector ? "green" : "unset",
-                    }}
-                    disabled={!triedEager || !!activatingConnector || connectorsList[name].connector === connector || !!error}
-                    onClick={() => {
-                      setActivatingConnector(connectorsList[name].connector);
-                      activate(connectorsList[name].connector);
-                    }}>
-                    <div>
-                      {connectorsList[name].connector === activatingConnector && <span>loading</span>}
-                      {connectorsList[name].connector === connector && (
-                        <span role="img" aria-label="check">
-                          ✅
-                        </span>
-                      )}
-                    </div>
-                    {name}
-                  </Button>
+          <Grid container spacing={3} >
+            {Object.keys(connectorsList).map((key) => (
+              <Grid item container alignContent="center" columnSpacing={3} rowSpacing={1} key={key}>
+                <Grid item xs={12} sm={5}>
+                  <Tooltip title={
+                    t(isCurrentConnector(key) ? 'base.connectedWith' : 'base.connectWith', {
+                      connector: connectorsList[key].name
+                      })
+                  }>
+                    <LoadingButton
+                      fullWidth
+                      disableElevation
+                      variant={isCurrentConnector(key) ? "contained" : "outlined"}
+                      size="large"
+                      color={isCurrentConnector(key) ? "success" : "primary"}
+                      startIcon={isCurrentConnector(key) ? <CheckIcon/> : <AccountBalanceWalletIcon />}
+                      loading={handleConnectorButtonLoadingProp(key)}
+                      disabled={handleConnectorButtonDisabledProp(key)}
+                      onClick={() => handleConnectorButtonClick(key)}
+                    >
+                      {connectorsList[key].name}
+                    </LoadingButton>
+                  </Tooltip>
                 </Grid>
-                <Grid item sm={6} md={8}>
-                  <Typography variant="body2">{connectorsList[name].description}</Typography>
+                <Grid item xs={12} sm={7} container alignContent="center" id="wallet-connectors-dialog-description">
+                  <Typography variant="body2">{connectorsList[key].description}</Typography>
                 </Grid>
               </Grid>
-            </Box>
-          ))}
-          <Grid container item spacing={2}>
-            {!!error && <h4 style={{ marginTop: "1rem", marginBottom: "0" }}>{getErrorMessage(error)}</h4>}
+            ))}
+            {!!error &&
+              <Grid item xs={12}>
+                <Typography variant="h6" color="error">
+                  {getErrorMessage(error)}
+                </Typography>
+              </Grid>
+            }
           </Grid>
         </DialogContent>
         <DialogActions>
