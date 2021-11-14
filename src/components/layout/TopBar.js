72119f0ce770bcb0fe4a1dc940f29237d40406d7
rolandsaven <rolandka@live.com>
2021-11-14 22:44:15 +0000

    clean up TopBar component for refactor

--- src/components/layout/TopBar.js
+++ src/components/layout/TopBar.js
@@ -1,71 +1,108 @@
 import React, { useState } from 'react'
-import { withTranslation } from "react-i18next";
+import { withTranslation } from 'react-i18next'
 import { Link, NavLink } from 'react-router-dom'
-// material-ui
-import Toolbar from "@mui/material/Toolbar";
-import AppBar from "@mui/material/AppBar";
-import Grid from "@mui/material/Grid";
-import Button from "@mui/material/Button";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemText from "@mui/material/ListItemText";
-import DialogActions from "@mui/material/DialogActions";
-import Dialog from "@mui/material/Dialog";
-import DialogTitle from "@mui/material/DialogTitle";
-import DialogContent from "@mui/material/DialogContent";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import Tooltip from "@mui/material/Tooltip";
-import Typography from "@mui/material/Typography";
-import Hidden from "@mui/material/Hidden";
-import IconButton from "@mui/material/IconButton";
-import Stack from "@mui/material/Stack";
-// icons
-import Settings from "@mui/icons-material/Settings";
-// custom
-import Web3connect from "./Web3connect";
-import LanguageSelector from "./LanguageSelector";
-import Logo from "../common/Logo";
-import LogoText from "../common/LogoText";
 import { useWeb3React } from '@web3-react/core'
+// material-ui
+import {
+  Toolbar,
+  AppBar,
+  Grid,
+  Button,
+  List,
+  ListItem,
+  ListItemText,
+  DialogActions,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  ListItemIcon,
+  Tooltip,
+  Typography,
+  Hidden,
+  IconButton,
+  Stack
+} from '@mui/material'
+// icons
+import Settings from '@mui/icons-material/Settings'
+// custom
+import Web3connect from './Web3connect'
+import LanguageSelector from './LanguageSelector'
+import Logo from '../common/Logo'
+import LogoText from '../common/LogoText'
 
 const links = [
-  { name: "Store", visible: true, disabled: false, path: "/store", description: "Buy Aroma and CHEF" },
-  { name: "Kitchen", visible: true, disabled: true, path: "/kitchen", description: "Create your own recipe" },
-  { name: "Buffet", visible: true, disabled: true, path: "/buffet", description: "Order and enjoy your meal" },
-  { name: "Collection", visible: active => active, disabled: false, path: "/collection", description: "See the collection of CHEFs" }
-];
+  {
+    name: 'Store',
+    visible: true,
+    disabled: false,
+    path: '/store',
+    description: 'Buy Aroma and CHEF',
+  },
+  {
+    name: 'Kitchen',
+    visible: true,
+    disabled: true,
+    path: '/kitchen',
+    description: 'Create your own recipe',
+  },
+  {
+    name: 'Buffet',
+    visible: true,
+    disabled: true,
+    path: '/buffet',
+    description: 'Order and enjoy your meal',
+  },
+  {
+    name: 'Collection',
+    visible: active => active,
+    disabled: false,
+    path: '/collection',
+    description: 'See the collection of CHEFs',
+  },
+]
 
 const NavLinkRef = React.forwardRef((props, ref) => (
     <NavLink {...props} ref={ref} className={({ isActive }) => (
-      props.className + (isActive ? " MuiButton-active" : "")
-    )} />
-  )
+      props.className + (isActive ? ' MuiButton-active' : '')
+    )}/>
+  ),
 )
 
-function TopBar ({t}) {
-  const [ popover, setPopover ] = useState(false)
+function TopBar ({ t }) {
+  const [popover, setPopover] = useState(false)
   const { active } = useWeb3React()
 
   const handleConnectionIconClick = () => {
     setPopover(true)
-  };
+  }
   const handleConnectionMenuClose = () => {
     setPopover(false)
-  };
+  }
 
   return (
     <AppBar elevation={0} position="static">
       <Toolbar variant="large">
         <Grid container justifyContent="space-between" alignItems="center">
-          <Grid item xs={6} md={4} container justifyContent="left" alignItems="center">
+          <Grid item container
+                xs={6}
+                md={4}
+                justifyContent="left"
+                alignItems="center"
+          >
             <Grid item xs="auto">
-              <Logo />
+              <Logo/>
             </Grid>
             <Grid item xs>
-              <LogoText />
+              <LogoText/>
             </Grid>
           </Grid>
-          <Grid item xs={6} md={8} container justifyContent="right" alignItems="center">
+          <Grid item
+                container
+                xs={6}
+                md={8}
+                justifyContent="right"
+                alignItems="center"
+          >
             <Grid item>
               <Hidden mdDown>
                 <Stack spacing={1} direction="row" mr={4}>
@@ -78,7 +115,7 @@ function TopBar ({t}) {
                     <Tooltip key={index} title={item.description}>
                         <span>
                           <Button
-                            elongatedwidth={item.disabled ? "false" : "true"}
+                            elongatedwidth={item.disabled ? 'false' : 'true'}
                             color="tertiary"
                             variant="contained"
                             disabled={item.disabled}
@@ -86,7 +123,9 @@ function TopBar ({t}) {
                             to={item.path}>
                             {item.name}
                             {item.disabled && (
-                              <Typography pl={0.5} variant="caption" sx={{opacity:0.7}}>
+                              <Typography pl={0.5} variant="caption"
+                                          sx={{ opacity: 0.7 }}
+                              >
                                 SOON
                               </Typography>
                             )}
@@ -97,34 +136,54 @@ function TopBar ({t}) {
                 </Stack>
               </Hidden>
               <Hidden mdUp>
-                <Tooltip title={t("base.navigation")}>
-                  <IconButton color="tertiary" onClick={handleConnectionIconClick}>
-                    <Settings />
+                <Tooltip title={t('base.navigation')}>
+                  <IconButton color="tertiary"
+                              onClick={handleConnectionIconClick}
+                  >
+                    <Settings/>
                   </IconButton>
                 </Tooltip>
-                <Dialog onClose={handleConnectionMenuClose} aria-labelledby="navigation" open={popover}>
-                  <DialogTitle id="navigation">{t("base.navigation")}</DialogTitle>
+                <Dialog onClose={handleConnectionMenuClose}
+                        aria-labelledby="navigation"
+                        open={popover}
+                >
+                  <DialogTitle id="navigation">{t(
+                    'base.navigation')}</DialogTitle>
                   <DialogContent>
                     <List dense>
                       {links.map((item, index) => (
-                        <ListItem key={index} button component={Link} disabled={item.disabled} to={item.path} onClick={handleConnectionMenuClose}>
-                          <ListItemIcon color="primary">{item.disabled ? "soon" : item.icon}</ListItemIcon>
-                          <ListItemText primary={item.name} secondary={item.description} />
+                        <ListItem key={index}
+                                  button
+                                  component={Link}
+                                  disabled={item.disabled}
+                                  to={item.path}
+                                  onClick={handleConnectionMenuClose}
+                        >
+                          <ListItemIcon color="primary">
+                            {item.disabled ? 'soon' : item.icon}
+                          </ListItemIcon>
+                          <ListItemText primary={item.name}
+                                        secondary={item.description}
+                          />
                         </ListItem>
                       ))}
                     </List>
                   </DialogContent>
                   <DialogActions>
-                    <Button disableElevation color="primary" variant="contained" onClick={handleConnectionMenuClose}>
-                      {t("base.close")}
+                    <Button disableElevation
+                            color="primary"
+                            variant="contained"
+                            onClick={handleConnectionMenuClose}
+                    >
+                      {t('base.close')}
                     </Button>
                   </DialogActions>
                 </Dialog>
               </Hidden>
             </Grid>
             <Grid item>
-              <Web3connect />
-              <LanguageSelector />
+              <Web3connect/>
+              <LanguageSelector/>
             </Grid>
           </Grid>
         </Grid>
@@ -134,4 +193,4 @@ function TopBar ({t}) {
 
 }
 
-export default withTranslation()(TopBar);
+export default withTranslation()(TopBar)
