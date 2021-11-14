510a1e7c02cc3661268f4494f54692cf319f6b14
rolandsaven <rolandka@live.com>
2021-11-14 11:15:39 +0000

    update LogoText component to white

--- src/components/common/LogoText.js
+++ src/components/common/LogoText.js
@@ -1,11 +1,11 @@
-import React from "react";
-import {Link} from 'react-router-dom'
-import { withTranslation } from "react-i18next";
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { withTranslation } from 'react-i18next'
 // material-ui
-import Tooltip from "@mui/material/Tooltip";
-import Button from "@mui/material/Button";
+import Tooltip from '@mui/material/Tooltip'
+import Button from '@mui/material/Button'
 // assets
-import logoText from '../../assets/logo-text.svg'
+import logoText from '../../assets/logo-text-white.svg'
 
 /**
  * @param t
@@ -15,14 +15,18 @@ import logoText from '../../assets/logo-text.svg'
 function LogoText ({ t }) {
   return (
     <Tooltip disableFocusListener
-             title={t("base.titleTooltip")}
-             aria-label={t("base.titleTooltip")}
+             title={t('base.titleTooltip')}
+             aria-label={t('base.titleTooltip')}
     >
-        <Button sx={{ maxWidth:"170px", paddingTop: "8px" }} disableRipple component={Link} to='/'>
-          <img style={{ width: "100%" }} alt="CryptoChefs logo" src={logoText} />
-        </Button>
+      <Button disableRipple
+              sx={{ maxWidth: '170px', paddingTop: '8px' }}
+              component={Link}
+              to="/"
+      >
+        <img style={{ width: '100%' }} alt="CryptoChefs logo" src={logoText}/>
+      </Button>
     </Tooltip>
-  );
+  )
 }
 
-export default withTranslation()(LogoText);
+export default withTranslation()(LogoText)
