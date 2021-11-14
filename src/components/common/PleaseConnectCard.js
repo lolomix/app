204f871d395f09bdff96bad8150f8a8fde392629
rolandsaven <rolandka@live.com>
2021-11-14 22:44:32 +0000

    add PleaseConnectCard component

---
+++ src/components/common/PleaseConnectCard.js
@@ -0,0 +1,33 @@
+import React from 'react'
+// material-ui
+import { Card, CardContent, Grid } from '@mui/material'
+import Typography from '@mui/material/Typography'
+// custom
+import bg from '../../assets/components/common/please-connect/bg.png'
+
+/**
+ * @param props
+ * @returns {JSX.Element}
+ * @constructor
+ */
+function PleaseConnectCard (props) {
+  return (
+    <Card {...props}>
+      <CardContent>
+        <Grid container justifyContent="center" alignItems="center">
+          <Grid item>
+            <img src={bg}
+                 alt="Crypto Wallet Providers"
+                 style={{ maxWidth: '182px' }}
+            />
+            <Typography mt={4} textAlign="center">
+              Please connect your wallet
+            </Typography>
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
+  )
+}
+
+export default PleaseConnectCard
