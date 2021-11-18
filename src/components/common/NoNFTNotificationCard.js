72b12d013b17994ceeb548c9f5f939d6e5c8082d
rolandsaven <rolandka@live.com>
2021-11-18 00:03:43 +0000

    add NoNFTNotificationCard Component

---
+++ src/components/common/NoNFTNotificationCard.js
@@ -0,0 +1,35 @@
+import React from 'react'
+// material-ui
+import { Card, CardContent, Grid, Link } from '@mui/material'
+import Typography from '@mui/material/Typography'
+// custom
+import chefs from '../../assets/components/common/no-nft-notification-card/chefs@2x.png'
+
+/**
+ * @param props
+ * @returns {JSX.Element}
+ * @constructor
+ */
+function NoNFTNotificationCard (props) {
+  const { rest } = props
+
+  return (
+    <Card {...rest}>
+      <CardContent>
+        <Grid container justifyContent="center" alignItems="center">
+          <Grid item textAlign="center">
+            <img src={chefs}
+                 alt="Chef Silhouettes"
+                 style={{ maxWidth: '182px' }}
+            />
+            <Typography mt={3}>
+              It seems that you do not own any CHEFs. <Link to="/store">Visit our Store</Link> to dive into our delicious word.
+            </Typography>
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
+  )
+}
+
+export default NoNFTNotificationCard
