2dbf5234bcfeaebe6eab743b5e91eb5b91fc1d41
rolandsaven <rolandka@live.com>
2021-11-18 15:57:20 +0000

    remove hard-coded tokenUri

--- src/hooks/useNFTWithMetadata.js
+++ src/hooks/useNFTWithMetadata.js
@@ -46,11 +46,6 @@ export function useNFTWithMetadata(tokenAbi, tokenAddress, tokenID) {
 
         let tokenURI = await contract.methods.tokenURI(tokenID).call({ from: account })
 
-        // todo: remove this hard coded ipfs link
-        if (true || ! isValidURL(tokenURI)) {
-          tokenURI = `ipfs://QmXj4xmXe9wHw3GTkCXHs9ihto3hgNhLy9Eh9sFsBEv1Nt/${tokenID}`;
-        }
-
         let metadata = await fetchMetadataByTokenURI(tokenURI)
 
         if (metadata.image && isValidURL(metadata.image)) {
