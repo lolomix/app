49ad5caab9fdd4a9dfc8a64a3bb5b3da1b1a2670
rolandsaven <rolandka@live.com>
2021-11-19 22:33:33 +0000

    add custom babel config for quick DX

---
+++ .babelrc.js
@@ -0,0 +1,15 @@
+const plugins = [
+  [
+    "babel-plugin-direct-import",
+    {
+      "modules": [
+        "@mui/lab",
+        "@mui/system",
+        "@mui/material",
+        "@mui/icons-material"
+      ]
+    }
+  ]
+];
+
+module.exports = { plugins };
