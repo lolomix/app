7e800994485cad36281db471ce59b23f93a8fc5d
rolandsaven <rolandka@live.com>
2021-11-19 20:03:27 +0000

    remove assetList.js

--- src/views/Kitchen/assetList.js
+++
@@ -1,5 +0,0 @@
-export const assetList = [
-  { code: "btc", name: "Bitcoin", description: "", active: true, icon: "./icons/btc.png" },
-  { code: "eth", name: "Ethereum",  description: "", active: true, icon: "./icons/eth.png"},
-  { code: "matic", name: "Matic",  description: "", active: true, icon: "./icons/matic.png" },
-];
