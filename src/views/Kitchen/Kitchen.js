61eb2b07a3809a2b5d655f77e04e076474eb73df
rolandsaven <rolandka@live.com>
2021-11-19 20:15:29 +0000

    refactor Kitchen page

--- src/views/Kitchen/Kitchen.js
+++ src/views/Kitchen/Kitchen.js
@@ -1,55 +1,38 @@
-import React, { Component } from "react";
+import React from "react";
 import { withTranslation } from "react-i18next";
-//import { Link } from "react-router-dom";
-//material-ui
-//import Typography from "@mui/material/Typography";
+// material-ui
 import Grid from "@mui/material/Grid";
-//import Card from "@mui/material/Card";
-//import CardActionArea from "@mui/material/CardActionArea";
-//import CardContent from "@mui/material/CardContent";
+import { Helmet } from 'react-helmet'
+import { Box, Container } from '@mui/material'
+// custom
+import Headline from '../../components/layout/Headline'
 import OpeningSoon from "../../components/common/OpeningSoon";
 
-class Kitchen extends Component {
-  componentDidMount() {
-    document.body.className = "kitchen";
-  }
-  render() {
-    return (
-      <Grid item xs={12} md={9} xl={6}>
-        <OpeningSoon />
-        {/* 
-        <Grid container spacing={2}>
-          <Grid item sm={12} md={6}>
-            <Card>
-              <CardActionArea component={Link} to="/market">
-                <CardContent>
-                  <Typography gutterBottom variant="h5">
-                    Become a chef
-                  </Typography>
-                  <Typography variant="body2">It looks like that you are not yet a chef. In order to create a recipe, you must acquire a chef NFT.</Typography>
-                  <Typography variant="body2">Click here to go the market and get a chef NFT.</Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
+/**
+ * @returns {JSX.Element}
+ * @constructor
+ */
+function Kitchen () {
+
+  return (
+    <>
+      <Helmet>
+        <title>Kitchen</title>
+      </Helmet>
+      <Box pb={10} pt={1} sx={{ backgroundColor: 'sunGlow.main' }}>
+        <Container as="section">
+          <Headline>
+            Kitchen
+          </Headline>
+          <Grid container justifyContent="center" alignItems="stretch">
+            <Grid item md={6} mb={21}>
+              <OpeningSoon/>
+            </Grid>
           </Grid>
-          <Grid item sm={12} md={6}>
-            <Card>
-              <CardActionArea component={Link} to="/buffet">
-                <CardContent>
-                  <Typography gutterBottom variant="h5">
-                    Go to the buffet
-                  </Typography>
-                  <Typography variant="body2">You don't want to be a chef yourself? Go directly to the buffet and check out the recipes out there.</Typography>
-                  <Typography variant="body2">Click here to go the buffet and spend some AROMA tokens.</Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          </Grid>
-        </Grid>
-        */}
-      </Grid>
-    );
-  }
+        </Container>
+      </Box>
+    </>
+  )
 }
 
-export default withTranslation()(Kitchen);
+export default withTranslation()(Kitchen)
