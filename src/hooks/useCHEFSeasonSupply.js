89fc686e0e3335fd84db8e92c2e6f4170e24860d
rolandsaven <rolandka@live.com>
2021-11-20 15:43:09 +0000

    add useCHEFSeasonSupply hook

---
+++ src/hooks/useCHEFSeasonSupply.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react'
+import { useWeb3React } from '@web3-react/core'
+import { NETWORKS, TARGET_CHAIN } from '../web3/constants'
+import tokenAbi from '../web3/abi/CryptoChefsERC721Facet.json'
+
+/**
+ * Returns the total amount of CHEFs in the Season.
+ *
+ * @returns {undefined}
+ */
+export function useCHEFSeasonSupply() {
+  const tokenAddress = NETWORKS[TARGET_CHAIN].contractMaster;
+
+  const { account, library } = useWeb3React()
+  const [seasonSupply, setSeasonSupply] = useState()
+
+  useEffect(() => {
+
+    // early return if no connection to blockchain
+    if (!account || !library) {
+      return
+    }
+
+    async function loadSeasonSupply() {
+      try {
+        const contract = new library.eth.Contract(tokenAbi, tokenAddress);
+        const seasonSupply = await contract.methods.getCryptoChefSeasonSupply().call();
+
+        setSeasonSupply(seasonSupply)
+
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    loadSeasonSupply();
+
+  }, [account, library, tokenAddress])
+
+  return seasonSupply
+}
+
