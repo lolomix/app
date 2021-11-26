7d0c7e8af8bf14f7bffe6524b8f64f66573bbe37
rolandsaven <rolandka@live.com>
2021-11-26 22:40:24 +0000

    refactor AromaBalance component to use hook

--- src/components/web3/AromaBalance.js
+++ src/components/web3/AromaBalance.js
@@ -1,42 +1,13 @@
-import React from "react";
-import { useWeb3React } from "@web3-react/core";
-//import { utils } from "web3";
-import Skeleton from "@mui/material/Skeleton";
-import { NETWORKS, TARGET_CHAIN, AROMA_DECIMALS } from "../../web3/constants";
-
+import React from 'react'
+import Skeleton from '@mui/material/Skeleton'
 // utils
-import {formatCurrency} from '../../utils/formatters'
+import { formatCurrency } from '../../utils/formatters'
+import { useAROMABalanceOf } from '../../hooks/useAROMABalanceOf'
 
 const AromaBalance = function () {
-  const { account, library } = useWeb3React();
-  const [balance, setBalance] = React.useState();
-  React.useEffect(() => {
-    if (!!account && !!library) {
-      async function loadBalance() {
-        try {
-          const contractAroma = NETWORKS[TARGET_CHAIN].contractAroma;
-          const minAbi = [
-            {
-              constant: true,
-              inputs: [{ name: "_owner", type: "address" }],
-              name: "balanceOf",
-              outputs: [{ name: "balance", type: "uint256" }],
-              type: "function",
-            },
-          ];
-          const contract = new library.eth.Contract(minAbi, contractAroma);
-          const balance = await contract.methods.balanceOf(account).call();
-          setBalance(balance / AROMA_DECIMALS);
-        } catch (e) {
-          console.log(e);
-        }
-      }
-      loadBalance();
-    }
-  }, [account, library]); // ensures refresh if referential identity of library doesn't change across chainIds
+  const balance = useAROMABalanceOf()
 
+  return balance === undefined ? <Skeleton variant="text"/> : <>{formatCurrency(balance)}</>
+}
 
-  return balance === undefined ? <Skeleton variant="text" /> : <>{formatCurrency(balance)}</>;
-};
-
-export default AromaBalance;
+export default AromaBalance
