e4ebe0adaadfec4cbf53784108664af7f401fffc
rolandsaven <rolandka@live.com>
2021-11-27 22:19:28 +0000

    abstracting MyAccountPopover component

--- src/components/layout/Web3connect.js
+++ src/components/layout/Web3connect.js
@@ -5,15 +5,9 @@ import { UnsupportedChainIdError, useWeb3React } from '@web3-react/core'
 //mui
 import {
   Tooltip,
-  List,
-  ListItem,
-  ListItemAvatar,
-  ListItemText,
   Button,
   Avatar,
   IconButton,
-  Box,
-  Stack,
   Dialog,
   DialogTitle,
   DialogContent,
@@ -24,27 +18,19 @@ import {
 import {
   AccountBalanceWallet,
   Check,
-  Warning,
 } from '@mui/icons-material'
 import { LoadingButton } from '@mui/lab'
 //custom
 import connectorsList from '../../web3/connectorsList'
 import { getErrorMessage } from '../../web3/errors'
 import { useEagerConnect } from '../../web3/hooks'
-import Balance from '../web3/Balance'
-import NFTIdList from '../web3/NFTIdList'
-import AromaBalance from '../web3/AromaBalance'
 import { NETWORKS, TARGET_CHAIN } from '../../web3/constants'
 import WalletMetaMaskIcon from '../icons/WalletMetaMaskIcon'
 import WalletLedgerIcon from '../icons/WalletLedgerIcon'
 import WalletWalletConnectIcon from '../icons/WalletWalletConnectIcon'
-import IconButtonVerifyExplorer from '../web3/IconButtonVerifyExplorer'
-import IconButtonCopy from '../IconButtonCopy'
-import CHEFAbi from '../../web3/abi/CryptoChefsERC721Facet.json'
+import MyAccountPopover from './MyAccountPopover'
 
 function Web3connect(props) {
-  const CHEFAddress = NETWORKS[TARGET_CHAIN].contractMaster
-
   const { t } = props;
 
   const Web3Context = useWeb3React();
@@ -54,14 +40,6 @@ function Web3connect(props) {
   const [activatingConnector, setActivatingConnector] = useState();
   const [connectionMenu, setConnectionMenu] = useState(false);
   const [dialogWeb3, setDialogWeb3] = useState(false);
-
-  /**
-   * Prettifies an ethereum address for presentation.
-   * @type {function(*): string}
-   *
-   * @todo refactor this to a global helper
-   */
-  const prettifyAccountAddress = (address) => `${address.slice(0, 6)}...${address.slice(-4)}`;
 
   const handleConnectionMenu = () => {
     setConnectionMenu(!connectionMenu);
@@ -190,69 +168,10 @@ function Web3connect(props) {
               </Avatar>
             </IconButton>
           </Tooltip>
-          <Dialog id="settings-menu" open={Boolean(connectionMenu)} onClose={handleConnectionMenu} maxWidth="md">
-            <Box p={2}>
-              <List dense>
-                {account && (
-                  <ListItem
-                    secondaryAction={
-                      <>
-                      <IconButtonVerifyExplorer address={account} />
-                      <IconButtonCopy copyText={account} />
-                      </>
-                    }>
-                    <Tooltip disableFocusListener title={t("base.youraccount")}>
-                      <ListItemAvatar>
-                        <Avatar>
-                          <Blockies seed={account.toLowerCase()} size={10} scale={4} className="blockies" />
-                        </Avatar>
-                      </ListItemAvatar>
-                    </Tooltip>
-                    <ListItemText secondary={prettifyAccountAddress(account.toLowerCase())} primary={t("base.yourAddress")} />
-                  </ListItem>
-                )}
-                {chainId === NETWORKS[TARGET_CHAIN].chainId ? (
-                  <ListItem>
-                    <ListItemAvatar>
-                      <Avatar className="avatar-success">
-                        <Check color="tertiary" />
-                      </Avatar>
-                    </ListItemAvatar>
-                    <ListItemText secondary={TARGET_CHAIN.toUpperCase() + " network"} primary="Connection Established" />
-                  </ListItem>
-                ) : (
-                  <ListItem>
-                    <ListItemAvatar>
-                      <Avatar className="avatar-warning">
-                        <Warning />
-                      </Avatar>
-                    </ListItemAvatar>
-                    <ListItemText secondary={"Please select " + TARGET_CHAIN.toUpperCase() + " in your wallet"} primary="WRONG NETWORK" />
-                  </ListItem>
-                )}
-                <ListItem>
-                  <ListItemText secondary={<AromaBalance />} primary="Your AROMA balance" />
-                </ListItem>
-                <ListItem>
-                  <ListItemText secondary={<Balance />} primary="Your MATIC balance" />
-                </ListItem>
-                <ListItem>
-                  <ListItemText secondary={<NFTIdList tokenAbi={CHEFAbi} tokenAddress={CHEFAddress}/>} primary="Your CHEF NFTs" />
-                </ListItem>
-                <ListItem>
-                  <ListItemText primary="Your CHEF NFTs will be revealed soon!" />
-                </ListItem>
-              </List>
-              <Stack spacing={1} direction="row" justifyContent="center">
-                <Button disableElevation color="primary" variant="contained" onClick={handleConnectionMenu}>
-                  {t("base.close")}
-                </Button>
-                <Button color="primary" variant="outlined" onClick={handleWeb3Modal}>
-                  Web3 Settings
-                </Button>
-              </Stack>
-            </Box>
-          </Dialog>
+          <MyAccountPopover connectionMenu={connectionMenu}
+                            handleWeb3Modal={handleWeb3Modal}
+                            handleConnectionMenu={handleConnectionMenu}
+          />
         </>
       )}
       <Dialog
