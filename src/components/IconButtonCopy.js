03e9e366d4b8200566ae14b1bf75bbeb91df138d
rolandsaven <rolandka@live.com>
2021-11-28 21:34:35 +0000

    refactor IconButtonCopy component

--- src/components/IconButtonCopy.js
+++ src/components/IconButtonCopy.js
@@ -5,8 +5,7 @@ import IconButton from "@mui/material/IconButton";
 import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 import CheckIcon from "@mui/icons-material/Check";
 
-
-function IconButtonCopy ({ copyText, size = 'medium', fontSize, t }) {
+function IconButtonCopy ({ t, copyText, size, fontSize }) {
 
   /**
    * Handles the copy to clipboard using Clipboard API OR regular method
@@ -15,48 +14,44 @@ function IconButtonCopy ({ copyText, size = 'medium', fontSize, t }) {
    */
   const handleAccountCopyToClipboardIconClick = () => {
     if (!success) {
-      setSuccess(true);
+      setSuccess(true)
     }
     // navigator clipboard api needs a secure context (https)
     if (navigator.clipboard && navigator.permissions) {
-      navigator.clipboard.writeText(copyText);
-    } else if (document.queryCommandSupported("copy")) {
-      const ele = document.createElement("textarea");
-      ele.value = copyText;
-      document.body.appendChild(ele);
-      ele.select();
-      document.execCommand("copy");
-      document.body.removeChild(ele);
+      navigator.clipboard.writeText(copyText)
+    } else if (document.queryCommandSupported('copy')) {
+      const ele = document.createElement('textarea')
+      ele.value = copyText
+      document.body.appendChild(ele)
+      ele.select()
+      document.execCommand('copy')
+      document.body.removeChild(ele)
     }
-  };
+  }
 
-
-  const [success, setSuccess] = useState(false);
+  const [success, setSuccess] = useState(false)
 
   useEffect(() => {
     if (success) {
-        setTimeout(() => {
-            setSuccess(false);
-          }, 1000);
-      }
-  }, [success]);
-
-
-  let iconProps = {
-    sx: {}
-  }
-
-  if (fontSize) {
-    iconProps.sx.fontSize = fontSize
-  }
+      setTimeout(() => {
+        setSuccess(false)
+      }, 1000)
+    }
+  }, [success])
 
   return (
-    <Tooltip title={success ? "copied" : t("base.copyToClipboard")}>
-        <IconButton onClick={handleAccountCopyToClipboardIconClick} size={size} edge="end" aria-label="copy">
-          {success ? <CheckIcon {...iconProps}/> : <ContentCopyIcon {...iconProps} />}
-        </IconButton>
+    <Tooltip title={success ? 'copied' : t('base.copyToClipboard')}>
+      <IconButton
+        component="a"
+        size={size}
+        onClick={handleAccountCopyToClipboardIconClick}
+        edge="end"
+        aria-label="Copy account address"
+      >
+        {success ? <CheckIcon sx={{ fontSize: fontSize }}/> : <ContentCopyIcon sx={{ fontSize: fontSize }}/>}
+      </IconButton>
     </Tooltip>
-  );
+  )
 }
 
 export default withTranslation()(IconButtonCopy);
