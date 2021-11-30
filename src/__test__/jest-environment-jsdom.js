0647f42afb78d5aaf3376fe32ae3eccff4a1b317
rolandsaven <rolandka@live.com>
2021-11-30 10:40:53 +0000

    fix TextEncoder bug

--- src/__test__/jest-environment-jsdom.js
+++ src/__test__/jest-environment-jsdom.js
@@ -18,7 +18,14 @@ class MyEnvironment extends JSDOMEnvironment {
     );
   }
 
-  async setup() {}
+  async setup() {
+    await super.setup();
+    if (typeof this.global.TextEncoder === 'undefined') {
+      const { TextEncoder, TextDecoder } = require('util');
+      this.global.TextEncoder = TextEncoder;
+      this.global.TextDecoder = TextDecoder;
+    }
+  }
 
   async teardown() {}
 }
