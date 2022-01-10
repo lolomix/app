329d227b8ade232c4800aff59375e629c46a3e59
rolandsaven <rolandka@live.com>
2022-01-10 09:58:35 +0000

    clean redundant web3 connectors

--- src/web3/connectorsList.js
+++ src/web3/connectorsList.js
@@ -1,17 +1,5 @@
 import {
-  injected,
-  // walletconnect,
-  // walletlink,
-  // ledger,
-  // network,
-  // trezor,
-  // lattice,
-  // frame,
-  // authereum,
-  // fortmatic,1
-  // magic,
-  // portis,
-  // torus
+  injected
 } from "./connectors";
 
 const connectorsList = {
@@ -37,38 +25,5 @@ const connectorsList = {
     connector: {}
   }
 };
-/*
-const ConnectorNames = {
-  Injected: "Injected",
-  Network: "Network",
-  WalletConnect: "WalletConnect",
-  WalletLink: "WalletLink",
-  Ledger: "Ledger",
-  Trezor: "Trezor",
-  Lattice: "Lattice",
-  Frame: "Frame",
-  Authereum: "Authereum",
-  Fortmatic: "Fortmatic",
-  Magic: "Magic",
-  Portis: "Portis",
-  Torus: "Torus",
-};
-
-export const connectorsByName = {
-  [ConnectorNames.Injected]: injected,
-  [ConnectorNames.Network]: network,
-  [ConnectorNames.WalletConnect]: walletconnect,
-  [ConnectorNames.WalletLink]: walletlink,
-  [ConnectorNames.Ledger]: ledger,
-  [ConnectorNames.Trezor]: trezor,
-  [ConnectorNames.Lattice]: lattice,
-  [ConnectorNames.Frame]: frame,
-  [ConnectorNames.Authereum]: authereum,
-  [ConnectorNames.Fortmatic]: fortmatic,
-  [ConnectorNames.Magic]: magic,
-  [ConnectorNames.Portis]: portis,
-  [ConnectorNames.Torus]: torus,
-};
-*/
 
 export default connectorsList;
