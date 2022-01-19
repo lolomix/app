e832a4513ec2e6b1b4ae1335e330fab0c122939b
rolandsaven <rolandka@live.com>
2022-01-19 15:32:55 +0000

    update main buttons on Home View

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,46 +1,43 @@
 import { withTranslation } from "react-i18next";
 import { Button, Stack } from "@mui/material";
 import RecipeOnPlateIcon from "../../components/icons/RecipeOnPlateIcon";
-import WhiteFlameIcon from "../../assets/white-flame-icon.svg";
 import SquareButton from "../../components/buttons/SquareButton";
 import Market from "../../assets/market-icon.svg";
 import MyChefs from "../../assets/my-chefs-icon.png";
 import Kitchen from "../../assets/kitchen-icon.svg";
 import Buffet from "../../assets/buffet-icon.svg";
+import WhiteFlameIcon from "../../components/icons/WhiteFlameIcon";
 
 function Home() {
   return (
     <Stack alignItems="center" spacing={3} mb="5vw">
       <RecipeOnPlateIcon sx={{ fontSize: 350 }} />
       <Button
+        elongatedWidth
+        size="massive"
         variant="yellowContained"
         href="/kitchen/recipe/create/"
-        style={{
-          width: "258px",
-          height: "60px",
-          fontSize: "21px",
-        }}
+        startIcon={<WhiteFlameIcon />}
       >
-        <img
-          src={WhiteFlameIcon}
-          alt="WhiteFlameIcon"
-          width="8%"
-          height="auto"
-          style={{ marginBottom: "5px", marginRight: "12px" }}
-        ></img>
         Cook A Recipe
       </Button>
       <Button
-        variant="yellowContained"
-        style={{
-          borderRadius: "25px",
-          background: "#FFFFFF",
+        elongatedWidth
+        variant="contained"
+        shape="roundish"
+        sx={{
+          backgroundColor: "common.white",
         }}
         href="/tutorial/"
       >
         Tutorial
       </Button>
-      <Stack direction="row" justifyContent="space-between" width="95%"  spacing={2}>
+      <Stack
+        direction="row"
+        justifyContent="space-between"
+        width="95%"
+        spacing={2}
+      >
         <Stack direction="row" spacing={2}>
           <SquareButton
             size={"large"}
