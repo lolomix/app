ab1fd7a176bb90ef4717fb4315d8144833e6c2f8
rolandsaven <rolandka@live.com>
2022-01-22 22:05:39 +0000

    add first step to KitchenRecipeCreate

--- src/views/KitchenRecipeCreate/KitchenRecipeCreate.jsx
+++ src/views/KitchenRecipeCreate/KitchenRecipeCreate.jsx
@@ -1,40 +1,23 @@
-import React from "react";
-import { withTranslation } from "react-i18next";
 // material-ui
 import Grid from "@mui/material/Grid";
 import { Helmet } from "react-helmet";
-import {
-  Box,
-  Button,
-  Card,
-  CardActions,
-  CardContent,
-  CardHeader,
-  Container,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogTitle,
-  Typography,
-} from "@mui/material";
+import { Box, Card, Container } from "@mui/material";
 // custom
-import ViewHeading from "../../components/layout/ViewHeading";
 import RecipeCreateStepper from "../../components/common/RecipeCreateStepper";
-import RecipeOnPlateIcon from "../../components/icons/RecipeOnPlateIcon";
+import TokenSelectorDialog from "../../components/dialogs/TokenSelectorDialog";
+import { useDialogState } from "../../hooks/useDialogState";
+import { bindDialog } from "../../utils/binders";
+import RecipeSelectTokensStep from "../../components/common/RecipeSelectTokensStep";
+import { RecipeCreatorProvider } from "../../contexts/recipeCreatorContext";
 
 /**
  * @returns {JSX.Element}
  * @constructor
  */
 function KitchenRecipeCreate() {
-  const [open, setOpen] = React.useState(false);
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-  const handleClose = () => {
-    setOpen(false);
-  };
+  const tokenSelectorDialogState = useDialogState({
+    dialogId: "tokenSelectorDialog",
+  });
 
   return (
     <>
@@ -43,61 +26,31 @@ function KitchenRecipeCreate() {
       </Helmet>
       <Box pb={10} pt={1}>
         <Container as="section">
-          {/* @todo maybe we could leave some ViewHeading? However, we need a back button for sure.  */}
-          <ViewHeading>Kitchen</ViewHeading>
-          <Grid mb={4} container justifyContent="center" alignItems="center">
-            <Grid item xs sm={8} md={6} textAlign="center">
-              <Card>
-                {/* Box used as CardContent adds an annoying padding */}
-                <Box p={2}>
-                  <RecipeCreateStepper />
-                </Box>
-              </Card>
+          <RecipeCreatorProvider>
+            <Grid mb={4} container justifyContent="center" alignItems="center">
+              <Grid item xs sm={8} md={6} textAlign="center">
+                <Card>
+                  {/* Box used as CardContent adds an annoying padding  */}
+                  <Box p={2}>
+                    <RecipeCreateStepper />
+                  </Box>
+                </Card>
+              </Grid>
             </Grid>
-          </Grid>
-          <Grid container justifyContent="center" alignItems="center">
-            <Grid item xs sm={6} md={4} textAlign="center">
-              <Card>
-                <CardHeader
-                  title={
-                    <Typography color="secondary" variant="h4">
-                      Please add tokens to your recipe
-                    </Typography>
-                  }
-                />
-                <CardContent>
-                  <RecipeOnPlateIcon sx={{ fontSize: "12rem" }} />
-                </CardContent>
-                <CardActions>
-                  <Button
-                    fullWidth
-                    shape="squarish"
-                    variant="contained"
-                    onClick={handleClickOpen}
-                  >
-                    Add Tokens
-                  </Button>
-                  {/* @todo abstraction of this Dialog Component */}
-                  <Dialog
-                    open={open}
-                    onClose={handleClose}
-                    aria-labelledby="customized-dialog-title"
-                  >
-                    <DialogTitle>Select a token</DialogTitle>
-                    <DialogContent>Content</DialogContent>
-                    <DialogActions>
-                      {/* @todo proper styling of button */}
-                      <Button onClick={handleClose}>Save changes</Button>
-                    </DialogActions>
-                  </Dialog>
-                </CardActions>
-              </Card>
-            </Grid>
-          </Grid>
+            {/* @todo: add the rest of the steps */}
+            <RecipeSelectTokensStep
+              tokenSelectorDialogState={tokenSelectorDialogState}
+            />
+            <TokenSelectorDialog
+              fullWidth
+              maxWidth="xs"
+              {...bindDialog(tokenSelectorDialogState)}
+            />
+          </RecipeCreatorProvider>
         </Container>
       </Box>
     </>
   );
 }
 
-export default withTranslation()(KitchenRecipeCreate);
+export default KitchenRecipeCreate;
