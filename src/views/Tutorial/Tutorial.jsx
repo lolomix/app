1afca809e34ff1515d3d424d87a829703b92c989
Natayra Santos <natayras@gmail.com>
2022-01-23 16:09:27 +0100

    Create Tutorial view

--- src/views/Tutorial/Tutorial.jsx
+++ src/views/Tutorial/Tutorial.jsx
@@ -7,76 +7,139 @@ import {
   Button,
   Typography,
   Container,
+  Box,
 } from "@mui/material";
 import ViewHeading from "../../components/layout/ViewHeading";
 import CurrencyAromaCartoonIcon from "../../components/icons/CurrencyAromaCartoonIcon";
 import BuffetWithSignsIcon from "../../components/icons/BuffetWithSignsIcon";
+import MarketIcon from "../../components/icons/MarketIcon";
+import MyChefsIcon from "../../components/icons/MyChefsIcon";
+import KitchenIcon from "../../components/icons/KitchenIcon";
 
 const Tutorial = () => {
   const steps = [
     {
       title: "Step 1",
       description: "Buy Aroma Coin and a CHEF from Market",
-      icon: <BuffetWithSignsIcon sx={{ fontSize: "150px" }} />,
+      icon: (
+        <Box sx={{ marginY: 1 }}>
+          <CurrencyAromaCartoonIcon
+            sx={{ marginBottom: 2.5, marginRight: 1 }}
+          />
+          <MarketIcon sx={{ fontSize: 52, marginLeft: 1 }} />
+          <MyChefsIcon
+            sx={{
+              fontSize: 35,
+              marginBottom: 2,
+              marginLeft: 1,
+              transform: "rotate(30deg)",
+            }}
+          />
+        </Box>
+      ),
     },
     {
       title: "Step 2",
       description: `Wait for Reveal. CHEFS Bought will be added under "MyChefs"`,
-      icon: <BuffetWithSignsIcon sx={{ fontSize: "150px" }} />,
+      icon: <MyChefsIcon sx={{ fontSize: 60, marginY: 1 }} />,
     },
     {
       title: "Step 3",
       description: `Once CHEF is revealed, use it in "Kitchen" to Cook a recipe`,
-      icon: <BuffetWithSignsIcon sx={{ fontSize: "150px" }} />,
+      icon: (
+        <Box sx={{ marginY: 1 }}>
+          <CurrencyAromaCartoonIcon
+            sx={{ marginBottom: 2.5, marginRight: 1 }}
+          />
+          <KitchenIcon sx={{ fontSize: 40, marginLeft: 1 }} />
+          <MyChefsIcon
+            sx={{
+              fontSize: 35,
+              marginBottom: 2,
+              marginLeft: 1,
+              transform: "rotate(30deg)",
+            }}
+          />
+        </Box>
+      ),
     },
     {
       title: "Step 4",
       description: `Your cooked recipe will be added to "Buffet" and see how it performs. You can collect reward from the Home itself`,
-      icon: <BuffetWithSignsIcon sx={{ fontSize: "150px" }} />,
+      icon: (
+        <BuffetWithSignsIcon
+          sx={{ fontSize: 140, marginTop: -4, marginBottom: -1 }}
+        />
+      ),
     },
   ];
 
   return (
     <Container maxWidth="sm">
       <ViewHeading title={"Tutorial"} />
-      <Card fullheight="true" sx={{ margin: 2 }}>
-        <Stack
-          spacing={1}
-          sx={{
-            padding: 2,
-            textAlign: "center",
-            alignItems: "center",
-          }}
-        >
-          <Typography variant="h4" color="secondary.main" textAlign="center">
+      <Card sx={{ marginY: 5 }}>
+        <Stack p={2} textAlign="center">
+          <Typography variant="h4" color="secondary">
             How to Get Started
           </Typography>
           {steps.map((step) => (
             <>
-              <Grid container item alignItems="center">
+              <Grid
+                container
+                item
+                justifyContent="center"
+                alignItems="center"
+                p={2}
+              >
                 <Grid
-                  container
                   item
-                  flexDirection="column"
-                  alignItems="flex-start"
-                  xl={8}
-                  md={8}
-                  sm={8}
-                  xs={12}
+                  xl={12}
+                  md={12}
+                  sm={12}
+                  order={{ xl: 1, md: 1, sm: 1, xs: 1 }}
                 >
-                  <Grid item>{step.title}</Grid>
-                  <Grid item textAlign="left">
-                    {step.description}
-                  </Grid>
+                  <Typography
+                    variant="h5"
+                    color="text.primary"
+                    textAlign="left"
+                  >
+                    {step.title}
+                  </Typography>
                 </Grid>
-                <Grid item xl={4} md={4} sm={4} xs={12}>
+                <Grid
+                  item
+                  xl={7}
+                  md={7}
+                  sm={7}
+                  order={{ xl: 2, md: 2, sm: 2, xs: 3 }}
+                >
+                  <Typography variant="h5" textAlign="justify">
+                    {step.description}
+                  </Typography>
+                </Grid>
+                <Grid
+                  item
+                  xl={5}
+                  md={5}
+                  sm={5}
+                  xs={12}
+                  order={{ xl: 3, md: 3, sm: 3, xs: 2 }}
+                >
                   {step.icon}
                 </Grid>
               </Grid>
               <Divider flexItem />
             </>
           ))}
-          <Button variant="contained">Go to Market</Button>
+          <Button
+            variant="yellowContained"
+            size="massive"
+            href="/market"
+            startIcon={<MarketIcon />}
+            sx={{ marginTop: 2 }}
+          >
+            Go to Market
+          </Button>
         </Stack>
       </Card>
     </Container>
