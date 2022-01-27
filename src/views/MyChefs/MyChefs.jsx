a176d908be6928d324f75065fe82d14d9bd6c2a7
Natayra Santos <natayras@gmail.com>
2022-01-27 11:17:52 +0100

    Create Buy CHEF card

--- src/views/MyChefs/MyChefs.jsx
+++ src/views/MyChefs/MyChefs.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import { withTranslation } from "react-i18next";
 // material-ui
-import { Box, Container, Grid, Skeleton } from "@mui/material";
+import {
+  Box,
+  Container,
+  Grid,
+  Skeleton,
+  CardContent,
+  Card,
+  Typography,
+  Button,
+  Stack,
+} from "@mui/material";
 // custom
-import { Helmet } from "react-helmet";
 import { useEthers } from "@usedapp/core";
 import ViewHeading from "../../components/layout/ViewHeading";
-import DripDivider from "../../components/layout/DripDivider";
+import { Helmet } from "react-helmet";
 import NftCard from "../../components/web3/NftCard";
 import FAQ from "../../components/common/FAQ";
 import NoNftNotificationCard from "../../components/common/NoNftNotificationCard";
@@ -14,7 +23,12 @@ import ConnectionErrorCard from "../../components/common/ConnectionErrorCard";
 import { useChefofOwner } from "../../hooks/useChefofOwner";
 import tokenAbi from "../../web3/abi/CryptoChefsERC721Facet.json";
 import { NETWORKS, TARGET_CHAIN } from "../../web3/constants";
-
+import MyChefsIcon from "../../components/icons/MyChefsIcon";
+import ChefApronIcon from "../../components/icons/ChefApronIcon";
+import CurrencyAromaCartoonIcon from "../../components/icons/CurrencyAromaCartoonIcon";
+import { formatCurrency } from "../../utils/formatters";
+import BuyChef from "../../assets/Buy_CHEF.svg";
+import { theme } from "../../utils/theme.js";
 /**
  * @returns {JSX.Element}
  * @constructor
@@ -28,84 +42,86 @@ function MyChefs() {
   const nfts = useChefofOwner();
 
   return (
-    <>
+    <Container maxWidth="xl">
       <Helmet>
-        <title>Collection</title>
+        <title>My Chefs</title>
       </Helmet>
-      <Box pb={10} pt={1} sx={{ backgroundColor: "sunGlow.main" }}>
-        <Container as="section">
-          <ViewHeading mb={0}>Collection</ViewHeading>
-          <ViewHeading variant="h5" mt={0} textAlign="center">
-            {active &&
-              (nfts ? (
-                <>Total CHEFs Owned: {nfts.length}</>
-              ) : (
-                <Skeleton variant="h5" width={220} sx={{ margin: "auto" }} />
-              ))}
-          </ViewHeading>
-          <Grid container justifyContent="center" alignItems="stretch">
-            {active ? (
-              nfts ? (
-                nfts.length ? (
-                  <Grid item md={12} lg={10} container spacing={5}>
-                    {nfts.map((tokenID, index) => (
-                      <Grid key={tokenID.toString()} item sm={12} md={6}>
-                        <a
-                          target="_blank"
-                          rel="noreferrer nofollow"
-                          href={`${NETWORKS[TARGET_CHAIN].openSeaLink}/${
-                            NETWORKS[TARGET_CHAIN].contractMaster
-                          }/${tokenID.toString()}`}
-                          style={{ textDecoration: "none" }}
-                        >
-                          <NftCard
-                            tokenAbi={tokenAbi}
-                            tokenAddress={tokenAddress}
-                            tokenID={tokenID.toNumber()}
-                            lazyLoad={index > 2}
-                          />
-                        </a>
-                      </Grid>
-                    ))}
-                  </Grid>
-                ) : (
-                  <Grid item md={6} mb={21}>
-                    <NoNftNotificationCard />
-                  </Grid>
-                )
-              ) : (
-                <Grid item md={6} mb={21}>
-                  <Skeleton variant="rectangular" height={350} />
-                </Grid>
-              )
-            ) : (
-              <Grid item xs={10} sm={7} md={5} lg={4} mb={21}>
-                <ConnectionErrorCard error={error} elevation={3} />
+      <ViewHeading
+        title="My Chefs"
+        subTitle={`You Own : ${nfts ? nfts.length : 0}`}
+        icon={<MyChefsIcon sx={{ fontSize: 70, paddingBottom: 2 }} />}
+      ></ViewHeading>
+      <Grid container item mt={5} spacing={3} justifyContent="center">
+        <Grid item xs={12} md={6} lg={3}>
+          <Card sx={{ boxShadow: theme.blurredShadows }}>
+            <CardContent>
+              <Stack textAlign="center" spacing={1}>
+                <Typography variant="h3" color="secondary" fontWeight={500}>
+                  Buy a CHEF
+                </Typography>
+                <Typography variant="h5">Season 1</Typography>
+                <Box
+                  sx={{
+                    background:
+                      "rgba(237,246,255, 0.33) 0% 0% no-repeat padding-box",
+                    boxShadow: "inset 0px 6px 8px rgba(237,246,255, 1)",
+                    padding: 1.5,
+                  }}
+                >
+                  <picture>
+                    <source srcSet={BuyChef} />
+                    <img
+                      style={{ minWidth: "80%", margin: 20 }}
+                      src={BuyChef}
+                      alt={"Buy CHEF"}
+                    />
+                  </picture>
+                  <Button
+                    variant="yellowContained"
+                    fullWidth
+                    size="xlarge"
+                    startIcon={<CurrencyAromaCartoonIcon />}
+                  >
+                    <Typography variant="h4">{formatCurrency(1000)}</Typography>
+                  </Button>
+                </Box>
+              </Stack>
+            </CardContent>
+          </Card>
+        </Grid>
+        {active ? (
+          nfts ? (
+            nfts.map((tokenID, index) => (
+              <Grid key={tokenID.toString()} item xs={12} md={6} lg={3}>
+                <a
+                  target="_blank"
+                  rel="noreferrer nofollow"
+                  href={`${NETWORKS[TARGET_CHAIN].openSeaLink}/${
+                    NETWORKS[TARGET_CHAIN].contractMaster
+                  }/${tokenID.toString()}`}
+                  style={{ textDecoration: "none" }}
+                >
+                  <NftCard
+                    tokenAbi={tokenAbi}
+                    tokenAddress={tokenAddress}
+                    tokenID={tokenID.toNumber()}
+                    lazyLoad={index > 2}
+                  />
+                </a>
               </Grid>
-            )}
-          </Grid>
-        </Container>
-      </Box>
-
-      <Box id="faq" pb={10} sx={{ backgroundColor: "secondary.main" }}>
-        <DripDivider variant={2} color="sunGlow.main" />
-        <Container as="section">
-          <ViewHeading variant="h2" color="secondary.contrastText">
-            FAQ
-          </ViewHeading>
-          <Grid
-            container
-            justifyContent="center"
-            alignItems="center"
-            spacing={2}
-          >
-            <Grid item xs={12} sm={10} md={8}>
-              <FAQ color="secondary.contrastText" />
+            ))
+          ) : (
+            <Grid item md={6} mb={21}>
+              <NoNftNotificationCard />
             </Grid>
+          )
+        ) : (
+          <Grid item xs={10} sm={7} md={5} lg={4} mb={21}>
+            <ConnectionErrorCard error={error} elevation={3} />
           </Grid>
-        </Container>
-      </Box>
-    </>
+        )}
+      </Grid>
+    </Container>
   );
 }
 
