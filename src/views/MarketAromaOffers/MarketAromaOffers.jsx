bc42e9e375250a3b09b46d983e9d3814138eadbe
Natayra Santos <natayras@gmail.com>
2022-01-27 14:27:21 +0100

    Add click on button Enter to aroma/buy page

--- src/views/MarketAromaOffers/MarketAromaOffers.jsx
+++ src/views/MarketAromaOffers/MarketAromaOffers.jsx
@@ -97,6 +97,7 @@ const MarketAromaOffers = () => {
                   size="massive"
                   variant="yellowContained"
                   sx={{ marginTop: 3 }}
+                  href={card.price === null && "/market/aroma/buy"}
                   startIcon={card.price && <CurrencyMaticIcon />}
                 >
                   {card.price ? formatCurrency(card.price) : "Enter"}
