39a156da55a34eeb1d4cef266d340147a8cc2a4f
rolandsaven <rolandka@live.com>
2022-02-01 17:38:08 +0000

    refactor useRecipeCoinPairIds hook

---
+++ src/hooks/useRecipeCoinPairIds.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import { useContractCall } from "@usedapp/core";
+import { NETWORKS, TARGET_CHAIN } from "../web3/constants";
+import abi from "../web3/abi/CryptoChefsERC721Facet.json";
+import { utils } from "ethers";
+
+/**
+ * Returns the available coin pair id-s in the contract
+ *
+ * @returns {any[]}
+ *
+ * @todo move formatted value to the first place in return array
+ */
+export function useRecipeCoinPairIds() {
+  const abiInterface = new utils.Interface(abi);
+  const address = NETWORKS[TARGET_CHAIN].contractMaster;
+
+  const [coinPairsFormatted, setCoinPairsFormatted] = useState();
+
+  const [coinPairIds] =
+    useContractCall({
+      abi: abiInterface,
+      address: address,
+      method: "getAllCoinPairs",
+      args: [],
+    }) ?? [];
+
+  useEffect(() => {
+    setCoinPairsFormatted(coinPairIds?.map((pair) => pair.toNumber()));
+  }, [coinPairIds]);
+
+  return [coinPairIds, coinPairsFormatted];
+}
