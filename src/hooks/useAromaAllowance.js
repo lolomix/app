bc801ee3643f3074a74a78adb3b841cc4b653e0b
rolandsaven <rolandka@live.com>
2022-02-03 13:06:02 +0000

    add useAromaAllowance hook

---
+++ src/hooks/useAromaAllowance.js
@@ -0,0 +1,38 @@
+import {
+  AROMA_DECIMALS_DIGIT,
+  NETWORKS,
+  TARGET_CHAIN,
+} from "../web3/constants";
+import { useEthers, useTokenAllowance } from "@usedapp/core";
+import { formatUnits } from "@ethersproject/units";
+import { useEffect, useState } from "react";
+
+/**
+ * Returns the allowance of an AROMA token for an address
+ *
+ * @param ownerAddress
+ * @param spenderAddress
+ * @returns {[]}
+ */
+
+export function useAromaAllowance(
+  ownerAddress,
+  spenderAddress = NETWORKS[TARGET_CHAIN].contractMaster
+) {
+  const tokenAddress = NETWORKS[TARGET_CHAIN].contractAroma;
+  const { account } = useEthers();
+
+  const allowance = useTokenAllowance(
+    tokenAddress,
+    ownerAddress ?? account,
+    spenderAddress
+  );
+  const [balanceFormatted, setBalanceFormatted] = useState();
+
+  useEffect(() => {
+    allowance &&
+      setBalanceFormatted(formatUnits(allowance, AROMA_DECIMALS_DIGIT));
+  }, [allowance]);
+
+  return [allowance, balanceFormatted];
+}
