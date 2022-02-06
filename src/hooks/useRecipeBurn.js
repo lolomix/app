2886be9b8a15a0b5960cc24457d0284eb63a1638
rolandsaven <rolandka@live.com>
2022-02-06 09:27:38 +0000

    add useRecipeBurn hook

---
+++ src/hooks/useRecipeBurn.js
@@ -0,0 +1,16 @@
+import { useContractFunction } from "@usedapp/core";
+import { NETWORKS, TARGET_CHAIN } from "../web3/constants";
+import abi from "../web3/abi/CryptoChefsERC721Facet.json";
+import { Contract } from "@ethersproject/contracts";
+
+/**
+ * @returns {{send, state, events, resetState}}
+ */
+export function useRecipeBurn() {
+  const address = NETWORKS[TARGET_CHAIN].contractMaster;
+  const contract = new Contract(address, abi);
+
+  return useContractFunction(contract, "burnRecipe", {
+    transactionName: "Burn a Recipe",
+  });
+}
