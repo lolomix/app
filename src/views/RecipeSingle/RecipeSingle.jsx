e3f6d99d51a839788a4cf4c32a1bc3639af9373f
rolandsaven <rolandka@live.com>
2022-02-11 09:55:36 +0000

    add recipe single view

--- src/views/RecipeSingle/RecipeSingle.jsx
+++ src/views/RecipeSingle/RecipeSingle.jsx
@@ -1,34 +1,53 @@
-import React, { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Grid from "@mui/material/Grid";
+import { theme } from "../../utils/theme";
 import { useParams } from "react-router-dom";
 import NftCard from "../../components/web3/NftCard";
-import tokenAbi from "../../web3/abi/CryptoChefsERC721Facet.json";
-import { NETWORKS, TARGET_CHAIN } from "../../web3/constants";
-import Grid from "@mui/material/Grid";
-import { Box, Card, CardContent, Container, Typography } from "@mui/material";
-import { theme } from "../../utils/theme";
 import { useRecipeById } from "../../hooks/useRecipeById";
+import { NETWORKS, TARGET_CHAIN } from "../../web3/constants";
+import tokenAbi from "../../web3/abi/CryptoChefsERC721Facet.json";
+import { useRecipeCoinPairs } from "../../hooks/useRecipeCoinPairs";
+import { Box, Card, CardContent, Skeleton, Typography } from "@mui/material";
 import RecipePerformanceChart from "../../components/charts/RecipePerformanceChart";
 import RecipeCreateTokenPresenter from "../../components/common/RecipeCreateTokenPresenter";
+import GradualStepperInputField from "../../components/form/GradualStepperInputField";
+import Layout from "../../components/layout/Layout";
 
 /**
  * @returns {JSX.Element}
  * @constructor
- *
- * @todo fix error handling
  */
 function RecipeSingle() {
+  const [, coinParsFormatted] = useRecipeCoinPairs();
   const { recipeId } = useParams();
-  const [recipe, recipeFormatted] = useRecipeById(recipeId);
+  const [, recipeFormatted] = useRecipeById(recipeId);
   const tokenAddress = NETWORKS[TARGET_CHAIN].contractMaster;
+  const [coinPairs, setCoinPairs] = useState();
 
+  /**
+   * Add symbols to bare coin pairs
+   */
   useEffect(() => {
-    console.log(recipe, recipeFormatted);
-  }, [recipe, recipeFormatted]);
+    if (!coinParsFormatted || !recipeFormatted?.coinPairs) {
+      return;
+    }
+
+    setCoinPairs(
+      recipeFormatted.coinPairs?.map((recipeCoinPair) => {
+        return {
+          ...recipeCoinPair,
+          symbol: coinParsFormatted.find(
+            (coinPair) => coinPair.id === recipeCoinPair.id
+          )?.symbol,
+        };
+      })
+    );
+  }, [coinParsFormatted, recipeFormatted]);
 
   return (
-    <Container>
-      <Grid container item mt={5} spacing={2} justifyContent="center">
-        <Grid item xs={12} md={8}>
+    <Layout>
+      <Grid container item mt={4} justifyContent="center">
+        <Grid item xs={12} sm={11} md={9} lg={7}>
           <Card sx={{ boxShadow: theme.blurredShadows }}>
             <CardContent>
               <Typography
@@ -40,7 +59,7 @@ function RecipeSingle() {
                 {recipeFormatted?.name}
               </Typography>
               <Grid container spacing={4} justifyContent="center">
-                <Grid item xs={8} sm={5} md={5}>
+                <Grid item xs={8} sm={6} md={5}>
                   <NftCard
                     variant="outlined"
                     tokenAbi={tokenAbi}
@@ -48,85 +67,99 @@ function RecipeSingle() {
                     tokenID={recipeFormatted?.chefId}
                   />
                 </Grid>
-                <Grid container item xs={12} sm={7} spacing={2}>
-                  <Grid item xs={6}>
+                <Grid container item xs={12} sm={6} md={7} spacing={1}>
+                  <Grid item xs={5}>
                     <Typography variant="h5">Launched</Typography>
                   </Grid>
                   <Grid item xs={6}>
                     <Typography variant="h6">
-                      {recipeFormatted?.date}
+                      {recipeFormatted?.date ?? <Skeleton />}
                     </Typography>
                   </Grid>
-                  <Grid item xs={6}>
+                  <Grid item xs={5}>
                     <Typography variant="h5">Total Earned</Typography>
                   </Grid>
                   <Grid item xs={6}>
-                    <Typography variant="h6">N/A</Typography>
+                    <Typography variant="h6">Coming Soon</Typography>
                   </Grid>
-                  <Grid item xs={6}>
+                  <Grid item xs={5}>
                     <Typography variant="h5">Staked</Typography>
                   </Grid>
                   <Grid item xs={6}>
                     <Typography variant="h6">
-                      {recipeFormatted?.stakedAroma} AROMA
+                      {recipeFormatted?.stakedAroma ? (
+                        recipeFormatted.stakedAroma + " AROMA"
+                      ) : (
+                        <Skeleton />
+                      )}
                     </Typography>
                   </Grid>
-                  <Grid item xs={6}>
+                  <Grid item xs={5}>
                     <Typography variant="h5">Daily</Typography>
                   </Grid>
                   <Grid item xs={6}>
-                    <Typography variant="h6">N/A</Typography>
+                    <Typography variant="h6">Coming Soon</Typography>
                   </Grid>
-                  <Grid item xs={6}>
+                  <Grid item xs={5}>
                     <Typography variant="h5">Weekly</Typography>
                   </Grid>
                   <Grid item xs={6}>
-                    <Typography variant="h6">N/A</Typography>
+                    <Typography variant="h6">Coming Soon</Typography>
                   </Grid>
-                  <Grid item xs={6}>
+                  <Grid item xs={5}>
                     <Typography variant="h5">Monthly</Typography>
                   </Grid>
                   <Grid item xs={6}>
-                    <Typography variant="h6">N/A</Typography>
+                    <Typography variant="h6">Coming Soon</Typography>
                   </Grid>
                 </Grid>
               </Grid>
-              <Typography variant="h6" textAlign="left" gutterBottom>
+              <Typography variant="h6" textAlign="left" mt={3} gutterBottom>
                 The overall performance of your recipe
               </Typography>
               <RecipePerformanceChart />
-              <Typography mt={4} variant="h6" textAlign="left" gutterBottom>
+              <Typography variant="h6" textAlign="left" mt={3} gutterBottom>
                 Selected Tokens
               </Typography>
               <Grid container spacing={3}>
-                {recipeFormatted?.coinPairs?.map((token) => (
-                  <Grid
-                    key={token.id}
-                    item
-                    container
-                    xs={12}
-                    sm={6}
-                    md={4}
-                    spacing={2}
-                    justifyContent="center"
-                    alignItems="center"
-                  >
-                    <Grid item xs={7} sm={12}>
-                      <Box position="relative">
-                        <RecipeCreateTokenPresenter token={token} />
-                      </Box>
+                {coinPairs?.length > 0 ? (
+                  coinPairs?.map((token) => (
+                    <Grid
+                      key={token.id}
+                      item
+                      container
+                      xs={12}
+                      sm={6}
+                      md={4}
+                      spacing={2}
+                      justifyContent="center"
+                      alignItems="center"
+                    >
+                      <Grid item xs={7} sm={12}>
+                        <Box position="relative">
+                          <RecipeCreateTokenPresenter token={token} />
+                        </Box>
+                      </Grid>
+                      <Grid item xs={3} sm={5} md={6} lg={4}>
+                        <GradualStepperInputField
+                          value={token.percentage}
+                          valueSuffix="%"
+                          hideSteppers
+                        />
+                      </Grid>
                     </Grid>
-                    <Grid item xs={5} sm={8.5} md={10} lg={8}>
-                      {token.percentage}
-                    </Grid>
+                  ))
+                ) : (
+                  <Grid item xs={12}>
+                    <Skeleton variant="rectangular" height="10rem" />
                   </Grid>
-                ))}
+                )}
               </Grid>
             </CardContent>
           </Card>
         </Grid>
       </Grid>
-    </Container>
+    </Layout>
   );
 }
 
