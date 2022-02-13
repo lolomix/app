1426274f9138e7e8d3c844f413ef58569f4c9575
rolandsaven <rolandka@live.com>
2022-02-13 23:24:47 +0000

    add loggers helper

---
+++ src/utils/loggers.js
@@ -0,0 +1,25 @@
+/**
+ * @param {array} results
+ * @param {array} calls
+ *
+ * @todo introduce more sophisticated logger for useCall hooks
+ * @todo add tests
+ */
+export function logUseCalls(results, calls) {
+  results?.forEach((result, idx) => {
+    if (!result?.error) {
+      return;
+    }
+    console.error(
+      `Error encountered calling '${calls?.[idx]?.method}' on ${calls?.[idx]?.contract.address}: ${result.error.message}`
+    );
+  });
+}
+
+/**
+ * @param {object|undefined} result
+ * @param {object|undefined} call
+ */
+export function logUseCall(result, call) {
+  logUseCalls([result], [call]);
+}
