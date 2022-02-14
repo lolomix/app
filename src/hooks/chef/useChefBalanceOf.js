1d80f430f97935b92f7c25894c40039622fa475a
rolandsaven <rolandka@live.com>
2022-02-14 01:41:07 +0000

    refactor useChefBalanceOf hook

--- src/hooks/chef/useChefBalanceOf.js
+++ src/hooks/chef/useChefBalanceOf.js
@@ -1,38 +1,34 @@
-import {
-  AROMA_DECIMALS_DIGIT,
-  NETWORKS,
-  TARGET_CHAIN,
-} from "../../web3/constants";
+import { NETWORKS, TARGET_CHAIN } from "../../web3/constants";
 import abi from "../../web3/abi/CryptoChefsERC721Facet.json";
-import { formatUnits } from "@ethersproject/units";
-import { useContractCall } from "@usedapp/core";
+import { useCall } from "@usedapp/core";
 import { utils } from "ethers";
-import { useEffect, useState } from "react";
+import { Contract } from "@ethersproject/contracts";
+import { logUseCall } from "../../utils/loggers";
 
 /**
  * Returns the balance of an CHEF NFTs for an address
  *
- * @param targetAccount
- * @returns {undefined}
+ * @param {string|undefined} targetAccount
+ * @returns {number|undefined}
  */
 export function useChefBalanceOf(targetAccount) {
   const abiInterface = new utils.Interface(abi);
   const address = NETWORKS[TARGET_CHAIN].contractMaster;
-  const [balanceFormatted, setBalanceFormatted] = useState();
+  const defaultResult = undefined;
 
-  const [balance] =
-    useContractCall(
-      targetAccount && {
-        abi: abiInterface,
-        address: address,
-        method: "balanceOf",
-        args: [targetAccount],
-      }
-    ) ?? [];
+  const call = targetAccount && {
+    contract: new Contract(address, abiInterface),
+    method: "balanceOf",
+    args: [targetAccount],
+  };
 
-  useEffect(() => {
-    balance && setBalanceFormatted(formatUnits(balance, AROMA_DECIMALS_DIGIT));
-  }, [balance]);
+  const result = useCall(call) ?? defaultResult;
 
-  return [balance, balanceFormatted];
+  logUseCall(result, call);
+
+  if (!result?.value) {
+    return defaultResult;
+  }
+
+  return result?.value?.[0]?.toNumber();
 }
