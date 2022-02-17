2a93b331c78cec3a8d9976df64d84fbf8cdfa019
Natayra Santos <natayras@gmail.com>
2022-02-17 15:21:48 +0100

    Change Main Menu to have only internal links and be a Popover

--- src/components/layout/NavigationMainMenuButton.jsx
+++ src/components/layout/NavigationMainMenuButton.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 // material-ui
-import { Dialog, Tooltip, Grid, Link, DialogContent } from "@mui/material";
+import { Tooltip, Grid, Link, Popover } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 // custom
 import NavigationButton from "./NavigationButton";
@@ -12,9 +12,20 @@ import BuffetIcon from "../icons/BuffetIcon";
 import { Link as RouterLink } from "react-router-dom";
 
 function NavigationMainMenuButton() {
-  const [open, setOpen] = useState(false);
+  const [anchorEl, setAnchorEl] = useState(null);
 
-  const externalLinks = [
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+
+  {
+    /*const externalLinks = [
     {
       title: "OpenSea",
       ref: "https://opensea.io/collection/cryptochefs-io",
@@ -35,7 +46,8 @@ function NavigationMainMenuButton() {
       title: "Discord",
       ref: "https://discord.com/invite/JufpFYBdKG/",
     },
-  ];
+  ];*/
+  }
 
   const views = [
     {
@@ -65,73 +77,68 @@ function NavigationMainMenuButton() {
     },
   ];
 
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
   return (
     <>
       <Tooltip disableFocusListener title="Menu" aria-label="Menu">
-        <NavigationButton icon={<MenuIcon />} onClick={handleClickOpen} />
+        <NavigationButton
+          icon={<MenuIcon />}
+          variant="contained"
+          onClick={handleClick}
+        />
       </Tooltip>
-      <Dialog
-        fullScreen
+      <Popover
         open={open}
+        anchorEl={anchorEl}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        onClose={handleClose}
         PaperProps={{
           sx: {
             backgroundColor: "common.black",
+            width: "fit-content",
           },
         }}
       >
-        <DialogContent
-          sx={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "flex-start",
-          }}
-        >
-          <Grid container maxWidth="xl">
-            <Grid item xs={12} textAlign="right">
-              <NavigationButton icon={<MenuIcon />} onClick={handleClose} />
-            </Grid>
-            <Grid
-              container
-              item
-              mt={8}
-              xs={12}
-              md={6}
-              flexDirection="column"
-              rowSpacing={5}
-            >
-              {views.map((item) => (
-                <Grid
-                  container
-                  item
-                  alignItems="center"
-                  columnSpacing={2}
-                  key={item.title}
-                >
-                  <Grid item>{item.icon}</Grid>
-                  <Grid item>
-                    <Link
-                      variant="h2"
-                      underline="hover"
-                      to={item.ref}
-                      component={RouterLink}
-                      color="common.white"
-                      fontWeight={500}
-                    >
-                      {item.title}
-                    </Link>
-                  </Grid>
+        <Grid container pl={3} pb={3}>
+          <Grid item xs={12} textAlign="right">
+            <NavigationButton
+              icon={<MenuIcon />}
+              variant="contained"
+              onClick={handleClose}
+            />
+          </Grid>
+          <Grid container item xs={12} flexDirection="column" rowSpacing={3}>
+            {views.map((item) => (
+              <Grid
+                container
+                item
+                alignItems="center"
+                columnSpacing={2}
+                key={item.title}
+              >
+                <Grid item>{item.icon}</Grid>
+                <Grid item>
+                  <Link
+                    variant="h2"
+                    underline="hover"
+                    to={item.ref}
+                    component={RouterLink}
+                    color="common.white"
+                    fontWeight={500}
+                  >
+                    {item.title}
+                  </Link>
                 </Grid>
-              ))}
-            </Grid>
-            <Grid
+              </Grid>
+            ))}
+          </Grid>
+          {/*<Grid
               container
               item
               mt={10}
@@ -158,10 +165,9 @@ function NavigationMainMenuButton() {
                   </Link>
                 </Grid>
               ))}
-            </Grid>
-          </Grid>
-        </DialogContent>
-      </Dialog>
+              </Grid>*/}
+        </Grid>
+      </Popover>
     </>
   );
 }
