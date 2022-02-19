7fb02472b2de3ac9bcbd49a39d8822f8b977939f
rolandsaven <rolandka@live.com>
2022-02-19 21:51:14 +0000

    CU-1z0gnj1 fix HorizontalChipList Component

--- src/components/form/HorizontalChipList.jsx
+++ src/components/form/HorizontalChipList.jsx
@@ -1,12 +1,12 @@
-import { Chip, Grid, IconButton, Skeleton } from "@mui/material";
+import { Box, Chip, Grid, IconButton, Skeleton } from "@mui/material";
 import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
 import RemoveCircleIcon from "@mui/icons-material/RemoveCircle";
 import { theme } from "../../utils/theme";
 import Icon from "react-crypto-icons";
 
 /**
- * @param list
- * @param onRemoveIconButtonClick
+ * @param {array} list
+ * @param {function(object)} onRemoveIconButtonClick
  * @returns {JSX.Element}
  * @constructor
  */
@@ -14,42 +14,44 @@ function HorizontalChipList({ list, onRemoveIconButtonClick }) {
   return (
     <>
       {list.length > 0 ? (
-        <Grid container flexWrap="wrap" columnSpacing={1} my={1.1}>
+        <Grid container flexWrap="wrap" spacing={1} my={1}>
           {list.map((item) => (
-            <Grid item key={item.id} position="relative">
-              <Chip
-                label={item.symbol}
-                variant="outlined"
-                shape="squarish"
-                icon={
-                  item.icon ? (
-                    <Icon name={item.icon} size={18} />
-                  ) : (
-                    <HelpOutlineIcon />
-                  )
-                }
-              />
-              <IconButton
-                onClick={() =>
-                  onRemoveIconButtonClick && onRemoveIconButtonClick(item)
-                }
-                size="xsmall"
-                variant="contained"
-                color="error"
-                inverseColor
-                sx={{
-                  position: "absolute",
-                  right: theme.spacing(-0.7),
-                  top: theme.spacing(-0.7),
-                }}
-              >
-                <RemoveCircleIcon fontSize="small" />
-              </IconButton>
+            <Grid item key={item.id}>
+              <Box position="relative">
+                <Chip
+                  label={item.symbol}
+                  variant="outlined"
+                  shape="squarish"
+                  icon={
+                    item.icon ? (
+                      <Icon name={item.icon} size={18} />
+                    ) : (
+                      <HelpOutlineIcon />
+                    )
+                  }
+                />
+                <IconButton
+                  onClick={() =>
+                    onRemoveIconButtonClick && onRemoveIconButtonClick(item)
+                  }
+                  size="xsmall"
+                  variant="contained"
+                  color="error"
+                  inverseColor
+                  sx={{
+                    position: "absolute",
+                    right: theme.spacing(-0.7),
+                    top: theme.spacing(-0.7),
+                  }}
+                >
+                  <RemoveCircleIcon fontSize="small" />
+                </IconButton>
+              </Box>
             </Grid>
           ))}
         </Grid>
       ) : (
-        <Skeleton width="100%" height={50} />
+        <Skeleton animation={false} width="100%" height={50} />
       )}
     </>
   );
