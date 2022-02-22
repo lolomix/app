a1b38230d9fdf100bd196c83ff9998daf773c5bc
Natayra Santos <natayras@gmail.com>
2022-02-22 11:52:52 +0100

    Fix unwanted changes

--- src/components/common/RecipeCreateReviewStep.jsx
+++ src/components/common/RecipeCreateReviewStep.jsx
@@ -106,7 +106,7 @@ function RecipeCreateReviewStep({ tokenSelectorDialogState }) {
                         size="xsmall"
                         variant="contained"
                         color="error"
-                        inverseColor
+                        inversecolor="inverse"
                         sx={{
                           position: "absolute",
                           right: theme.spacing(-0.7),
@@ -156,7 +156,7 @@ function RecipeCreateReviewStep({ tokenSelectorDialogState }) {
               size="large"
               onClick={nextStep}
             >
-              {account ? "Next Step" : "Please Connect Wallet"}
+              {account ? "Next Step" : "Connect Wallet"}
             </Button>
           </CardActions>
         </Card>
