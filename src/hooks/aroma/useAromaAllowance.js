20273b99a41850515fcb472f28a599a3e278ffd1
rolandsaven <rolandka@live.com>
2022-03-05 10:32:31 +0000

    fine-tune AROMA approval

--- src/hooks/aroma/useAromaAllowance.js
+++ src/hooks/aroma/useAromaAllowance.js
@@ -1,13 +1,16 @@
 import { NETWORKS } from "../../web3/constants";
-import { useConfig, useEthers, useTokenAllowance } from "@usedapp/core";
-import { formatUnits } from "@ethersproject/units";
+import { useCall, useConfig, useEthers } from "@usedapp/core";
+import { formatUnits, parseUnits } from "@ethersproject/units";
+import { Contract } from "@ethersproject/contracts";
+import { logUseCall } from "../../utils/loggers";
+import { ERC20Interface } from "@usedapp/core";
 
 /**
  * Returns the allowance of an AROMA token for an address
  *
  * @param {string|undefined} ownerAddress
  * @param {string|undefined} spenderAddress
- * @returns [{string|undefined},{BigNumber|undefined}]
+ * @returns {{}|{bnAllowance: any, allowance: (any|string), hasAllowance: (function(*=))}}
  */
 export function useAromaAllowance(
   ownerAddress = undefined,
@@ -16,13 +19,36 @@ export function useAromaAllowance(
   const { readOnlyChainId } = useConfig();
   const tokenAddress = NETWORKS[readOnlyChainId].contractAroma;
   const { account } = useEthers();
+  const defaultResult = {
+    allowance: undefined,
+    bnAllowance: undefined,
+    hasAllowance: () => undefined,
+  };
+
   spenderAddress = spenderAddress || NETWORKS[readOnlyChainId].contractMaster;
+  ownerAddress = ownerAddress || account;
 
-  const allowance = useTokenAllowance(
-    tokenAddress,
-    ownerAddress ?? account,
-    spenderAddress
-  );
+  const call = {
+    contract: new Contract(tokenAddress, ERC20Interface),
+    method: "allowance",
+    args: [ownerAddress, spenderAddress],
+  };
 
-  return [allowance && formatUnits(allowance), allowance];
+  const result = useCall(call) ?? defaultResult;
+
+  logUseCall(result, call);
+
+  if (!result?.value) {
+    return defaultResult;
+  }
+
+  return {
+    allowance: result?.value?.[0] && formatUnits(result?.value?.[0]),
+    bnAllowance: result?.value?.[0],
+    hasAllowance: (amount) => {
+      amount = (amount && parseFloat(amount)) || Number.MAX_SAFE_INTEGER;
+      const bnAmount = parseUnits(amount.toString());
+      return bnAmount && result?.value?.[0]?.gte(bnAmount);
+    },
+  };
 }
