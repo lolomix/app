7d1f9f3f68d8858637063ede5b7b460b8ec3ab3b
rolandsaven <rolandka@live.com>
2022-03-13 11:23:36 +0000

    refactor from Grid to List

--- src/views/Buffet/Buffet.jsx
+++ src/views/Buffet/Buffet.jsx
@@ -1,20 +1,21 @@
-import { useState, Fragment, useEffect } from "react";
+import { useState, useEffect } from "react";
 import {
   Box,
   Button,
   TabUnstyled,
   TabsUnstyled,
-  TabPanelUnstyled,
   TabsListUnstyled,
   tabUnstyledClasses,
   CardActions,
   Card,
   CardContent,
   Typography,
-  Divider,
   Grid,
-  Link,
-  Skeleton,
+  List,
+  ListItemButton,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
 } from "@mui/material";
 import Layout from "../../components/layout/Layout";
 import BuffetIcon from "../../components/icons/BuffetIcon";
@@ -24,9 +25,9 @@ import { Link as RouterLink } from "react-router-dom";
 import { theme } from "../../utils/theme";
 import { useRecipesOfOwner } from "../../hooks/recipe/useRecipesOfOwner";
 import { ChefImageById } from "../../components/common/ChefImage";
-import useRecipeBlockchainAll from "../../hooks/recipe/useRecipeBlockchainAll";
-import useRecipePerformanceAll from "../../hooks/recipe/useRecipeBlockchainAll";
-import { useChefIdsOfOwner } from "../../hooks/chef/useChefIdsOfOwner";
+import useRecipePerformanceAll from "../../hooks/backend/recipe/useRecipePerformanceAll";
+import useRecipeBlockchainAll from "../../hooks/backend/recipe/useRecipeBlockchainAll";
+import { coinPairExplode } from "../../utils/helpers";
 
 const MY_RECIPES_FILTER = "my-recipes";
 const ALL_RECIPES_FILTER = "all-recipes";
@@ -108,13 +109,12 @@ const TabsList = styled(TabsListUnstyled)`
 function Buffet() {
   const { data: allRecipes } = useRecipeBlockchainAll();
   const { data: allRecipesPerformance } = useRecipePerformanceAll();
-  const chefIds = useChefIdsOfOwner();
+  const myRecipes = useRecipesOfOwner();
 
   const [recipesToShow, setRecipesToShow] = useState();
   const [myRecipesWithPerformance, setMyRecipesWithPerformance] = useState();
   const [allRecipesWithPerformance, setAllRecipesWithPerformance] = useState();
   const [recipesFilter, setRecipesFilter] = useState(ALL_RECIPES_FILTER);
-
 
   useEffect(() => {
     if (recipesFilter !== MY_RECIPES_FILTER) return;
@@ -129,39 +129,37 @@ function Buffet() {
   }, [JSON.stringify(allRecipesWithPerformance), recipesFilter]);
 
   useEffect(() => {
-    console.log("test");
     setMyRecipesWithPerformance(
-      allRecipes
-        ?.filter((recipe) =>
-          chefIds?.some((chefId) => recipe.chefId && chefId.eq(recipe.chefId))
-        )
-        ?.map((recipe) => {
-          return {
-            ...recipe,
-            ...allRecipesPerformance?.find(
-              (findRecipe) => findRecipe?.recipeId == recipe?.recipeId
-            ),
-          };
-        })
-    );
-  }, [
-    JSON.stringify(allRecipesPerformance),
-    JSON.stringify(allRecipes),
-    JSON.stringify(chefIds),
-  ]);
-
-  useEffect(() => {
-    console.log("test2");
-    setAllRecipesWithPerformance(
-      allRecipes?.map((recipe) => {
+      myRecipes?.map((recipe) => {
         return {
           ...recipe,
           ...allRecipesPerformance?.find(
-            (findRecipe) => findRecipe?.recipeId == recipe?.recipeId
+            (findRecipe) => findRecipe?.recipeId === recipe?.id
           ),
         };
       })
     );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [JSON.stringify(allRecipesPerformance), JSON.stringify(myRecipes)]);
+
+  useEffect(() => {
+    setAllRecipesWithPerformance(
+      allRecipes
+        ?.map((recipe) => {
+          return {
+            ...recipe,
+            ...allRecipesPerformance?.find(
+              (findRecipe) => findRecipe?.recipeId === recipe?.recipeId
+            ),
+          };
+        })
+        ?.sort(
+          (a, b) =>
+            (+b.overallPerformancePriceChangePercent || -99999) -
+            (+a.overallPerformancePriceChangePercent || -99999)
+        )
+    );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [JSON.stringify(allRecipesPerformance), JSON.stringify(allRecipes)]);
 
   const handleClickMyRecipes = () => {
@@ -171,10 +169,6 @@ function Buffet() {
   const handleClickAllRecipes = () => {
     setRecipesFilter(ALL_RECIPES_FILTER);
   };
-
-  useEffect(() => {
-    console.log(recipesToShow);
-  }, [recipesToShow]);
 
   return (
     <Layout
@@ -198,8 +192,14 @@ function Buffet() {
             variant="contained"
             size="xlarge"
             sx={{
-              backgroundColor: recipesFilter === MY_RECIPES_FILTER ? "common.white" : "#4B6272",
-              color: recipesFilter === MY_RECIPES_FILTER ? "common.black" : "common.white",
+              backgroundColor:
+                recipesFilter === MY_RECIPES_FILTER
+                  ? "common.white"
+                  : "#4B6272",
+              color:
+                recipesFilter === MY_RECIPES_FILTER
+                  ? "common.black"
+                  : "common.white",
               boxShadow: "none",
               "&:hover, &:active": {
                 color: "common.black",
@@ -214,8 +214,14 @@ function Buffet() {
             variant="contained"
             size="xlarge"
             sx={{
-              backgroundColor: recipesFilter === ALL_RECIPES_FILTER ? "common.white" : "#4B6272",
-              color: recipesFilter === ALL_RECIPES_FILTER ? "common.black" : "common.white",
+              backgroundColor:
+                recipesFilter === ALL_RECIPES_FILTER
+                  ? "common.white"
+                  : "#4B6272",
+              color:
+                recipesFilter === ALL_RECIPES_FILTER
+                  ? "common.black"
+                  : "common.white",
               boxShadow: "none",
               "&:hover, &:active": {
                 color: "common.black",
@@ -255,78 +261,85 @@ function Buffet() {
                     <Typography>Seems like you have no recipes...</Typography>
                   </Box>
                 )}
-                {recipesToShow &&
-                  recipesToShow?.length > 0 &&
-                  recipesToShow?.map((recipe) => (
-                    <Fragment key={recipe.recipeId || recipe.id}>
-                      <TabPanelUnstyled value={0} />
-                      <TabPanelUnstyled value={1}>
-                        <Link
-                          rel="noreferrer nofollow"
-                          href={`/buffet/recipe/${
-                            recipe.recipeId || recipe.id
-                          }`}
-                          style={{
-                            textDecoration: "none",
-                          }}
-                        >
-                          <Grid
-                            container
-                            py={2.5}
-                            alignItems="center"
-                            sx={{
-                              margin: "0.3vw",
-                              "&:hover": {
-                                backgroundColor: "grey.200",
-                                borderRadius: `${theme.shape.borderRadius}px`,
-                              },
-                            }}
-                          >
-                            <Grid item mr={2}>
-                              <Box
-                                sx={{
-                                  width: "75px",
-                                  minWidth: "75px",
-                                  border: "2px solid #E8E8E8",
-                                  borderRadius: `${theme.shape.borderRadius}px`,
-                                  paddingX: 0.5,
-                                }}
-                              >
-                                <ChefImageById tokenId={recipe.chefId} />
-                              </Box>
-                            </Grid>
-                            <Grid item xs={6} md={8.2} xl={10} textAlign="left">
-                              <Typography
-                                variant="h5"
-                                color="common.black"
-                                sx={{ padding: 1 }}
-                              >
-                                {recipe.name || recipe.recipeName}
-                              </Typography>
-                            </Grid>
-                            <Grid item xs={2} md={1.5}>
-                              <Typography
-                                variant="h5"
-                                color={
-                                  recipe?.overallPerformancePriceChangePercent >
+                <List>
+                  {recipesToShow &&
+                    recipesToShow?.length > 0 &&
+                    recipesToShow?.map((recipe) => (
+                      <ListItem
+                        key={recipe.recipeId || recipe.id}
+                        disablePadding
+                        secondaryAction={
+                          <Typography
+                            variant="h5"
+                            color={
+                              !recipe?.overallPerformancePriceChangePercent
+                                ? "secondary"
+                                : recipe?.overallPerformancePriceChangePercent <=
                                   0
-                                    ? "success.main"
-                                    : "secondary"
-                                }
-                              >
-                                {recipe?.overallPerformancePriceChangePercent?.toFixed(
+                                ? "error.main"
+                                : "success.main"
+                            }
+                          >
+                            {recipe?.overallPerformancePriceChangePercent
+                              ? recipe?.overallPerformancePriceChangePercent?.toFixed(
                                   2
-                                )}
-                                %
+                                ) + "%"
+                              : "New"}
+                          </Typography>
+                        }
+                        sx={{
+                          "&>.MuiListItemButton-root": {
+                            pr: 12,
+                          },
+                        }}
+                      >
+                        <ListItemButton
+                          to={`/buffet/recipe/${recipe.recipeId || recipe.id}`}
+                          component={RouterLink}
+                        >
+                          <ListItemAvatar sx={{ pr: 2 }}>
+                            <Box
+                              position="relative"
+                              width="85px"
+                              border={`2px solid ${theme.palette.grey.A200}`}
+                              borderRadius={`${theme.shape.borderRadius}px`}
+                            >
+                              <ChefImageById
+                                wrapperProps={{
+                                  sx: {
+                                    py: 0.5,
+                                    px: 1,
+                                  },
+                                }}
+                                imgProps={{ sizes: "5vw" }}
+                                tokenId={recipe.chefId}
+                              />
+                              <Typography
+                                as="div"
+                                textAlign="center"
+                                variant="overline"
+                                color="grey.A400"
+                                fontWeight="bold"
+                                position="absolute"
+                                lineHeight="1"
+                                bottom="1px"
+                                width="100%"
+                              >
+                                #{recipe.chefId}
                               </Typography>
-                            </Grid>
-                          </Grid>
-                        </Link>
-                        <Divider />
-                      </TabPanelUnstyled>
-                      <TabPanelUnstyled value={2}></TabPanelUnstyled>
-                    </Fragment>
-                  ))}
+                            </Box>
+                          </ListItemAvatar>
+                          <ListItemText
+                            primary={recipe.name || recipe.recipeName}
+                            primaryTypographyProps={{ fontWeight: "bold" }}
+                            secondary={recipe?.coinPairs
+                              ?.map((cp) => coinPairExplode(cp.symbol)?.[0])
+                              ?.join(", ")}
+                          />
+                        </ListItemButton>
+                      </ListItem>
+                    ))}
+                </List>
                 {recipesFilter === MY_RECIPES_FILTER && (
                   <Button
                     bg="yellowContained"
